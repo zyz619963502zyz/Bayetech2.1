69073f7b07a96624fd1e473160d98cb1090ce76d
dwj <jie@fhey.me>
2017-10-10 16:57:24 +0800

    入口文件提交更改

--- Bayetech.Web/Scripts/app/HelpCenter/helpCenter.js
+++ Bayetech.Web/Scripts/app/HelpCenter/helpCenter.js
@@ -1,20 +1,41 @@
-﻿//底部模板1
-define(['vue', 'jquery', 'head', 'foot','helpLeft', 'helpButtom', 'helpContent'], function () {
-    var html = `<div class="center">
-            <help-left></help-left>
-            <div class="help_right">
-                <help-content></help-content>
-                <help-buttom></help-buttom>
-            </div>
-        </div>`;
-    var components = {
-        name: "helpCenter",
-        template: html,
-        components: {},
-    };
+﻿//帮助中心主模块
+//define(['vue', 'jquery', 'head', 'foot', 'helpLeft', 'helpButtom', 'helpContent'], function () {
+//    var html = `<div class="center">
+//            <help-left></help-left>
+//            <div class="help_right">
+//                <help-content></help-content>
+//                <help-buttom></help-buttom>
+//            </div>
+//        </div>`;
+//    var components = {
+//        name: "helpCenter",
+//        template: html,
+//        components: {},
+//    };
+//    for (var i = 4; i < arguments.length; i++) {
+//        components["components"][arguments[i].name] = arguments[i];
+//    }
+//    return components;
+//});
+
+//启动文件
+require(['vue', 'jquery', 'common', 'head', 'foot', 'helpLeft', 'helpButtom', 'helpContent'], function (Vue, $, common, head, foot, helpCenter) {
+    //路由配置
+    let routeconfig = [{
+        path: webUrl + 'Page/HelpCenter/index.html',
+        component: helpCenter,
+    }];
+    Vue.component('main-head', head);
+    Vue.component('main-foot', foot);
+
+    var components = {};
     for (var i = 4; i < arguments.length; i++) {
-        components["components"][arguments[i].name] = arguments[i];
+        components[arguments[i].name] = arguments[i];
     }
-    return components;
+    
+    let app = new Vue({
+        components: components,
+    }).$mount('#app');
 });
+
 
