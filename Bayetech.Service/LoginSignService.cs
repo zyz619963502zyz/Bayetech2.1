59f880b43d96595875bac704c7e80f020d5e8a8d
zhaoyz5 <619963502@qq.com>
2017-10-11 09:52:25 +0800

    提交接口代码

---
+++ Bayetech.Service/LoginSignService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Bayetech.DAL;
+using Newtonsoft.Json;
+using Bayetech.DAL.Entity;
+
+namespace Bayetech.Service
+{
+    public class LoginSignService : ILoginSignService
+    {
+        
+        public int CreatAccount(JObject json)
+        {
+            RepositoryBase RBase = new RepositoryBase();
+
+            using (BayetechEntities entity = new BayetechEntities())
+            {
+                Account _account = (Account)JsonConvert.DeserializeObject(json["model"].ToString(), typeof(Account));
+                return RBase.Insert(_account);
+            }
+        }
+        public int CheckAccount(string account)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
