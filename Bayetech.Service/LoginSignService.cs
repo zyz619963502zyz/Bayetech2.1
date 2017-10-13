612b5d285d66dea6c97c66fa29c0cf4f4761d50a
zhaoyz5 <619963502@qq.com>
2017-10-13 11:42:10 +0800

    删除多余的using

--- Bayetech.Service/LoginSignService.cs
+++ Bayetech.Service/LoginSignService.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json.Linq;
 using Bayetech.DAL;
 using Newtonsoft.Json;
 using Bayetech.DAL.Entity;
 
 namespace Bayetech.Service
-{ 
+{
     public class LoginSignService : ILoginSignService
     {
         /// <summary>
