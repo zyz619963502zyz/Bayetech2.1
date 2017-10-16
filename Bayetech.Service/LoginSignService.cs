35ea50c6d06b1a381011838bc13b1673239f9681
zhaoyz5 <619963502@qq.com>
2017-10-16 13:44:02 +0800

    修改注册代码

--- Bayetech.Service/LoginSignService.cs
+++ Bayetech.Service/LoginSignService.cs
@@ -20,6 +20,7 @@ namespace Bayetech.Service
             using (var db = new RepositoryBase().BeginTrans())
             {
                 Account _account = (Account)JsonConvert.DeserializeObject(json.ToString(), typeof(Account));
+                _account.GamePass = Md5.EncryptString(_account.GamePass); 
                 int count = db.Insert(_account);
                 db.Commit();
                 return count > 0 ? true : false;
