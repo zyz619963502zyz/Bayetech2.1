d06281c71172fabddbcde102b7cfe6257ca731bc
zhaoyz5 <619963502@qq.com>
2017-10-24 18:10:56 +0800

    提交登录

--- Bayetech.Web/Scripts/app/Login/index.js
+++ Bayetech.Web/Scripts/app/Login/index.js
@@ -1,22 +1,22 @@
-﻿require(jsconfig.baseArr, function (Vue, $) {
+﻿require(jsconfig.baseArr, function ($, Vue, common) {
     var _loginUrl = "/api/Account/LoginIn"
 
     //登录的数据
-    var _data = {
-        GameUser: "",
-        GamePass: "",
-    }
+    //var _data = {
+    //    GameUser: "",
+    //    GamePass: "",
+    //}
 
     new Vue({
         el: '#LoginMain',
-        data: _data,
-        created: function () {
-
+        data: {
+            GameUser: "1111",
+            GamePass: "2222",
         },
         methods:{
             SubmitLogin: function () {
-                postWebJson(_loginUrl, JSON.stringify(this.data), function () {
-                    var a = 1;
+                common.postWebJson(_loginUrl, JSON.stringify(this.data), function (data) {
+                    alert(data == true ? "登录成功" : "账号或密码错误!");
                 });
             }
         }
