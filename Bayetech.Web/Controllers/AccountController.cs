0a9e1aa5c3377671a99759d6f2b37f70920fb6a3
zhaoyz5 <619963502@qq.com>
2017-10-24 23:36:15 +0800

    删除多余的引用

--- Bayetech.Web/Controllers/AccountController.cs
+++ Bayetech.Web/Controllers/AccountController.cs
@@ -1,21 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Security.Claims;
-using System.Security.Cryptography;
-using System.Threading.Tasks;
-using System.Web;
 using System.Web.Http;
-using System.Web.Http.ModelBinding;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin.Security;
-using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.OAuth;
-using Bayetech.Web.Models;
-using Bayetech.Web.Providers;
-using Bayetech.Web.Results;
 using Bayetech.Service;
 using Newtonsoft.Json.Linq;
 
--- Bayetech.Web/Controllers/PointTradingController.cs
+++ Bayetech.Web/Controllers/PointTradingController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace Bayetech.Web.Controllers
+﻿namespace Bayetech.Web.Controllers
 {
     public class PointTradingController : BaseController
     {
