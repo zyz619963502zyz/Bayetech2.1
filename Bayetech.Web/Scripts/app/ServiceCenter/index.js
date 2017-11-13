8016852b28dfa460704e6cb3c43937a648bed921
zgy <78695@DESKTOP-28FL8RR>
2017-11-14 00:07:08 +0800

    zgy

---
+++ Bayetech.Web/Scripts/app/ServiceCenter/index.js
@@ -0,0 +1,12 @@
+﻿var PersonalHeadModule = ['vue', 'jquery', 'common', 'Pavigation']
+//个人中心主模块
+require(PersonalHeadModule, function (Vue, $, common, pavi) {
+    var vm = new Vue({
+        el: '#PerHeadDiv',
+        data: {},
+        components: {
+            'regpavigation': pavi,
+        }
+    });
+
+});
