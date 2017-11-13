c94b286fc3538c5dcfa6dae22685f73b515caf8a
zgy <78695@DESKTOP-28FL8RR>
2017-11-13 22:36:13 +0800

    zgy

---
+++ Bayetech.Web/Scripts/app/PersonalCenter/content.js
@@ -0,0 +1,50 @@
+﻿//头部模板
+jsconfig.baseArr.push('PersonRight');
+jsconfig.baseArr.push('PersonButtom');
+jsconfig.baseArr.push('helpLeft');
+define(jsconfig.baseArr, function ($, Vue, common, perR, perB, helpleft) {
+    alert(1);
+    let html = `
+         <div class ="center">
+               <regleft :list="articleList"></regleft>
+                <div class ="personal_right">
+                    <regperright></regperright>
+                    <regpersonbuttom></regpersonbuttom>
+                </div>
+        </div>
+        `
+
+    let findListUrl = "/api/Article/FindList"; //查询列表
+
+    //表头数据
+    var data = {
+        mouduleid: "23",//帮助中心id
+        articleList: []
+    }
+    var components11 = {
+        template: html,
+        components: {
+            "regleft": helpleft,
+            "regperright": perR,
+            "regpersonbuttom": perB,
+        },
+        data: function () {
+            return data;
+        },
+        created: function () {
+            this.findList();
+        },
+        nowVue: this,
+        methods: {
+            findList() {
+                let nowVue = this;
+                common.getWebJson(findListUrl, { value: this.mouduleid }, function (data) {
+                    nowVue.articleList = data;
+                });
+            },
+        },
+    };
+
+
+    return components11;
+});
--- Bayetech.Web/Scripts/app/PersonalCenter/index.js
+++ Bayetech.Web/Scripts/app/PersonalCenter/index.js
@@ -1,55 +1,23 @@
-﻿var PersonalHeadModule = ['vue', 'jquery', 'common', 'PersonalHead', 'Pavigation', 'helpLeft', 'PersonRight', 'PersonButtom']
+﻿var PersonalHeadModule = ['vue', 'jquery', 'common', 'PersonalHead', 'Pavigation', 'Percontent']
 //个人中心主模块
-require(PersonalHeadModule, function (Vue, $, common, perHead, pavi, left,perRight,perButtom) {
-
-    let html = `
-         <div class ="center">
-                <div class ="personal_right">
-                    <PersonRight></PersonRight>
-                    <PersonButtom></PersonButtom>
-                </div>
-        </div>
-        `
+require(PersonalHeadModule, function (Vue, $, common, perHead, pavi, Percon) {
     //api
-    let findListUrl = "/api/Article/FindList"; //查询列表
-
-    //表头数据
-    var data = {
-        mouduleid: "23",//帮助中心id
-        articleList: []
-    }
-
     //注册主键
-    Vue.component('regperhead', perHead);
-    Vue.component('regpavigation', pavi);
-    Vue.component('regleft', left);
-    Vue.component('regperright', perRight);
-    Vue.component('regpersonbuttom', perButtom);
+    //Vue.component('regperhead', perHead);
+    //Vue.component('regpavigation', pavi);
+    //Vue.component('regleft', left);
+    //Vue.component('regperright', perRight);
+    //Vue.component('regpersonbuttom', perButtom);
+    //Vue.component('regcontent', Percon);
+
     var vm = new Vue({
         el: '#PerHeadDiv',
-        data: function () {
-            return data;
-        },
-        created: function () {
-            this.findList();
-        },
-        nowVue: this,
-        methods: {
-            findList() {
-                let nowVue = this;
-                common.getWebJson(findListUrl, { value: this.mouduleid }, function (data) {
-                    nowVue.articleList = data;
-                });
-            },
-        },
+        data:{},
+        components: {
+            'regperhead': perHead,
+            'regpavigation':pavi,
+            'aaaaa': Percon,
+        }
     });
-
-    var components = {
-        template: html
-
-
-    };
-
-    return components;
 
 });
