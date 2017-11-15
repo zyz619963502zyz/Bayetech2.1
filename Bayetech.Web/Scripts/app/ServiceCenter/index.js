630fd999731af19e03a575d2eae0a56c9691f768
zgy <78695@DESKTOP-28FL8RR>
2017-11-15 23:56:55 +0800

    安全中心

--- Bayetech.Web/Scripts/app/ServiceCenter/index.js
+++ Bayetech.Web/Scripts/app/ServiceCenter/index.js
@@ -1,11 +1,52 @@
-﻿var PersonalHeadModule = ['vue', 'jquery', 'common', 'Pavigation']
+﻿var PersonalHeadModule = ['vue', 'jquery', 'common', 'Pavigation', 'PersonalHead', 'ServiceLogo', 'ServiceNav', 'ServiceButtom']
 //个人中心主模块
-require(PersonalHeadModule, function (Vue, $, common, pavi) {
+require(PersonalHeadModule, function (Vue, $, common, pavi, servicehead, serlogo, servicenav,serbuttom) {
+    var data = {
+        ServiceList: [{ url: "http://www.7881.com/tradesafe.html", text: "首页" }
+                , { url: "http://www.7881.com/tradesafe.html", text: "安全知识中心" }
+                , { url: "http://www.7881.com/tradesafe.html", text: "验证中心" }
+                , { url: "http://www.7881.com/tradesafe.html", text: "帮助中心" }
+                , { url: "http://www.7881.com/tradesafe.html", text: "资讯中心" }
+        ],
+        ServiceNewList: [
+            {
+                title: "安全常识",
+                content: [{ url: "http://www.7881.com/tradesafe/825689321558028350a0.html", text: "看2014ChinaJoy了......" },
+                            { url: "http://www.7881.com/tradesafe/825689321558028350a0.html", text: "防范钓鱼网站的基本知识" },
+                            { url: "http://www.7881.com/tradesafe/825689321558028350a0.html", text: "什么是钓鱼网站" },
+                            { url: "http://www.7881.com/tradesafe/825689321558028350a0.html", text: "骗子诈骗流程" },
+                            { url: "http://www.7881.com/tradesafe/825689321558028350a0.html", text: "防止盗号木马盗取我们的账号和......" }]
+            },
+            {
+                title: "钓鱼网站盗号",
+                content: [{ url: "http://www.7881.com/tradesafe/825689321558028350a0.html", text: "7881平台建议" },
+                            { url: "http://www.7881.com/tradesafe/825689321558028350a0.html", text: "新闻背景：识破假冒钓鱼网站骗......" },
+                            { url: "http://www.7881.com/tradesafe/825689321558028350a0.html", text: "什么是钓鱼网站？" },
+                            { url: "http://www.7881.com/tradesafe/825689321558028350a0.html", text: "钓鱼网站的危害方式" },
+                            { url: "http://www.7881.com/tradesafe/825689321558028350a0.html", text: "卖家钓鱼后，冒充客服继续行骗......" }]
+            },
+            {
+                title: "假客服欺诈",
+                content: [{ url: "http://www.7881.com/tradesafe/825689321558028350a0.html", text: "案例：骗子们用一些荒诞无稽的......" },
+                            { url: "http://www.7881.com/tradesafe/825689321558028350a0.html", text: "低价游戏币换来的高价“担保金......" },
+                            { url: "http://www.7881.com/tradesafe/825689321558028350a0.html", text: "购买低价商品被钓鱼后资金被盗" },
+                            { url: "http://www.7881.com/tradesafe/825689321558028350a0.html", text: "如何识别真假客服" },
+                            { url: "http://www.7881.com/tradesafe/825689321558028350a0.html", text: "骗子利用花言巧语对新用户进行......" }]
+            }
+        ]
+
+    }
     var vm = new Vue({
-        el: '#PerHeadDiv',
-        data: {},
+        el: '#ServiceDiv',
+        data: function () {
+            return data;
+        },
         components: {
             'regpavigation': pavi,
+            'regservicehead': servicehead,
+            'regservicelogo': serlogo,
+            'regservicenav': servicenav,
+            'regservicebuttom': serbuttom,
         }
     });
 
---
+++ Bayetech.Web/Scripts/app/ServiceCenter/servicenew.js
@@ -0,0 +1,33 @@
+﻿//头部模板
+define(jsconfig.baseArr, function (Vue, $, common) {
+    debugger;
+    var html = `
+         <div class ="center">
+        <div class ="security_news" v-for="item in list">
+            <h3>{{item.title}}</h3>
+            <ul>
+                <li v-for="it in item.content">
+                    <a :href="it.url" target="_blank">
+                        {{it.text}}
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class ="security_index_right">
+            <ul>
+                <li>
+                <a href="http://www.7881.com/tradesafe/knowledge.html" title="安全知识中心"><img src="../../Content/Images/security_index_right_1.jpg"></a>
+                </li>
+                <ol>
+                    <a href="http://www.7881.com/tradesafe/safeCenter.html" title="验证中心"><img src="../../Content/Images/security_index_right_2.jpg"></a>
+                </ol>
+            </ul>
+        </div>
+    </div>
+        `
+    var components = {
+        template: html,
+        props: ['list'],
+    };
+    return components;
+});
