69d53419423a388f97b75e1e3a83274a70d3f5be
dongwj5 <dongwj5@SHCYF58T>
2017-11-22 09:49:11 +0800

    提交

--- Bayetech.Core/Common/PredicateExtensions.cs
+++ Bayetech.Core/Common/PredicateExtensions.cs
@@ -4,24 +4,51 @@ using System.Linq.Expressions;
 
 namespace Bayetech.Core
 {
+    /// <summary>
+    /// lambda 表达式动态拼接
+    /// </summary>
     public static class PredicateExtensions
     {
+        /// <summary>
+        /// True
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public static Expression<Func<T, bool>> True<T>()
         {
             return f => true;
         }
 
+        /// <summary>
+        /// False
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public static Expression<Func<T, bool>> False<T>()
         {
             return f => false;
         }
 
+        /// <summary>
+        /// 合集
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expression1"></param>
+        /// <param name="expression2"></param>
+        /// <returns></returns>
         public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expression1, Expression<Func<T, bool>> expression2)
         {
             var invokedExpression = Expression.Invoke(expression2, expression1.Parameters.Cast<Expression>());
             return Expression.Lambda<Func<T, bool>>(Expression.Or(expression1.Body, invokedExpression), expression1.Parameters);
         }
 
+        /// <summary>
+        /// 交集
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expression1"></param>
+        /// <param name="expression2"></param>
+        /// <returns></returns>
         public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expression1, Expression<Func<T, bool>> expression2)
         {
             var invokedExpression = Expression.Invoke(expression2, expression1.Parameters.Cast<Expression>());
