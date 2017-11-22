7aaf3748b0e7a893c74fa1bd2f17bc56bc74412f
dongwj5 <dongwj5@SHCYF58T>
2017-11-22 09:45:40 +0800

    1、提交咨询建议数据 2、提交lambda 表达式拼接 方法

--- Bayetech.Web/Scripts/app/Home/index.js
+++ Bayetech.Web/Scripts/app/Home/index.js
@@ -1,12 +1,13 @@
 ﻿//模块之间的操作
-require(['vue', 'jquery', 'common','bootstrap','swiper', 'v-header', 'v-footer', 'footer-server', 'footer-nav', 'index-notice', 'index-convenience', 'index-hotgamelist', 'index-banner', 'index-tabslist', 'v-tab', "index-gameranking", 'index-adv', 'index-mgamelist', 'v-partner', 'index-nav', 'v-search'],
-	function (Vue, $, common, bootstrap, Swiper, head, footer, footerserver, footernav, notice, convenience, hotgamelist, banner, tabsListbox, tab, gameranking, adv, mgamelist, partner, nav, search) {
+require(['vue', 'jquery', 'common','bootstrap','swiper', 'v-header', 'v-footer', 'footer-server', 'footer-nav', 'index-notice', 'index-consult','index-convenience', 'index-hotgamelist', 'index-banner', 'index-tabslist', 'v-tab', "index-gameranking", 'index-adv', 'index-mgamelist', 'v-partner', 'index-nav', 'v-search'],
+	function (Vue, $, common, bootstrap, Swiper, head, footer, footerserver, footernav, notice, consult, convenience, hotgamelist, banner, tabsListbox, tab, gameranking, adv, mgamelist, partner, nav, search) {
      
     new Vue({
         el: '#app',
         data() {
             return {
                 notice: notice,
+                consult:consult,
                 gameranking: gameranking,
             }
         },
