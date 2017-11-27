673fbfbc4d78312ed80f0570fec4ebbfab75159e
zgy <78695@DESKTOP-28FL8RR>
2017-11-27 22:45:27 +0800

    zgy

--- Bayetech.Web/Scripts/app/ServiceCenter/servicepusht.js
+++ Bayetech.Web/Scripts/app/ServiceCenter/servicepusht.js
@@ -2,17 +2,13 @@
 define(jsconfig.baseArr, function (Vue, $, common) {
     var html = `
             <div>
-                <div class ="qList pusht clearfix">
-                    <h3>常见问题</h3>
+                <div class ="qList pusht clearfix" v-for="item in object">
+                    <h3>{{item.title}}</h3>
                     <ul>
-                        <li><span class ="num">1</span><a href="" target="_blank">{{}}</a></li>
-                        <li><span class ="num">2</span><a href="http://www.7881.com/helpcenter/248692316587202013.html" target="_blank">如何联系订单客服</a></li>
-                        <li><span class ="num">3</span><a href="http://www.7881.com/helpcenter/248692203167715790.html" target="_blank">如何验证真假客服</a></li>
-                        <li><span class ="num">4</span><a href="http://www.7881.com/helpcenter/134344990756337236.html" target="_blank">如何正确出售商品</a></li>
-                        <li><span class ="num">5</span><a href="http://www.7881.com/helpcenter/73815958199886129.html" target="_blank">收货角色名如何修改</a></li>
-                        <li><span class ="num">6</span><a href="http://www.7881.com/helpcenter/1.html" target="_blank">如何提现我的资金</a></li>
-                        <li><span class ="num">7</span><a href="http://www.7881.com/helpcenter/268873779487604761.html" target="_blank">提现怎么收取手续费</a></li>
-                        <li><span class ="num">8</span><a href="http://www.7881.com/helpcenter/248692281206556395.html" target="_blank">订单撤销后退款到哪</a></li>
+                        <li v-for="it in item.content">
+                            <span class ="num">{{it.id}}</span>
+                            <a :href="it.url" target="_blank">{{it.text }}</a>
+                        </li>
                     </ul>
                 </div>
             </div>
