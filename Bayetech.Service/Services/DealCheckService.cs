9c6a8763c9d11f6d15aafd5e3cad94a916989ecf
zhaoyz5 <619963502@qq.com>
2017-11-27 23:34:25 +0800

    提交商品信息ID

--- Bayetech.Service/Services/DealCheckService.cs
+++ Bayetech.Service/Services/DealCheckService.cs
@@ -21,7 +21,7 @@ namespace Bayetech.Service.Services
                 var _good = new vw_MallGoodInfo();
                 if (!string.IsNullOrEmpty(goodNo))
                 {       
-                    _good = db.FindEntity<vw_MallGoodInfo>(c => c.GoodNo == goodNo);
+                    _good = db.FindEntity<vw_MallGoodInfo>(c => c.GoodInfoId == goodNo);
                     if (_good!=null)
                     {
                         ret.Add(ResultInfo.Result, JProperty.FromObject(true));
