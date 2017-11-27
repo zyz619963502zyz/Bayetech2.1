2cae2a097672cfc382c19303c12cab3a6f3075c9
zhaoyz5 <619963502@qq.com>
2017-11-27 23:47:38 +0800

    提交GoodInfo实体类。

---
+++ Bayetech.Web/Models/MallGoodInfoModels.cs
@@ -0,0 +1,16 @@
+﻿using Bayetech.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// 商品信息基础vo类，所有的订单信息等信息继承此类
+/// </summary>
+namespace Bayetech.Web.Models
+{
+    public class MallGoodInfoModels : MallGoodInfo
+    {
+        public List<vw_MallGoodInfo> mallGoodInfo;
+    }
+}
