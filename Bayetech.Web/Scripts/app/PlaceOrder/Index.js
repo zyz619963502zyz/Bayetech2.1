509549f671ffe0ba844632d8840403a733e5dc3a
zhaoyz5 <619963502@qq.com>
2017-12-05 14:20:13 +0800

    提交组件的修改

--- Bayetech.Web/Scripts/app/PlaceOrder/Index.js
+++ Bayetech.Web/Scripts/app/PlaceOrder/Index.js
@@ -1,16 +1,37 @@
 ﻿require(jsconfig.baseArr, function (Vue, $, common) {
     var vm = new Vue({
         el: '#PlaceOrderDiv',
-        created(){
+        created() {
             this.GetGoodInfo(common.GetUrlParam("", "GoodNo"));//获取商品信息。
         },
         data: {
-            FirstStep: {},
-            SecondStep: {},
-            ThirdStep: {}
+            FirstStep: {
+                BaseUrl: common.GetBaseUrl() + "PlaceOrder/PlaceOrder.html?GoodNo=",
+                AddTime: "",
+                GoodNo: "",
+                GoodTitle: "",
+                GoodPrice: 280,
+                GoodTypeName: "",
+                GroupName: "",
+                ServerName: "",
+                TotalNum: "",
+                CancelNum: "",
+                wantPriceUrl: "http://search.7881.com/201612376390646.html#"
+            },
+            SecondStep: {
+
+            },
+            ThirdStep: {
+
+            }
         },
         methods: {
 
-       }
+        },
+        components: {
+            'accountorder': account,
+            'goldorder': gold,
+            'cashorder': cash
+        }
     });
 });
