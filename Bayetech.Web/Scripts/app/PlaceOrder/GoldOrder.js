765675a2c9f978f24258b48f060443b5f90afb18
zhaoyz5 <619963502@qq.com>
2017-12-05 16:16:49 +0800

    提交金币交易

--- Bayetech.Web/Scripts/app/PlaceOrder/GoldOrder.js
+++ Bayetech.Web/Scripts/app/PlaceOrder/GoldOrder.js
@@ -6,23 +6,23 @@ define(jsconfig.baseArr, function (Vue, $, common) {
 
     //模板
     var goldorderHtml = `<ul id="GoldBuy">
-                            <li class="buyNum">
-                                <span>购买数量：<strong style="color:#F00">* </strong></span><input type="hidden"/>{{BuyNum}}
-                                <p class="txcpdd_default"> (可购1件)</p>
-                            </li>
-                            <li>
-                                <span>交易方式：<strong style="color:#F00">* </strong></span>
-                                <input type="radio" name="goodsPurchaseBea" value="face" checked="checked" />&nbsp; 当面交易
-                                <div style="border:solid red 1px; background:#f4faff;padding:5px;line-height:20px; margin-left:95px;color: red">
-                                    请拍单后前往16线行会莎兰处进行交易
-                                </div>
-                            </li>
-                            <li><span>所在区服：<strong style="color:#F00">* </strong></span>{{GameName}}/{{GroupName}}/{{ServerName}}</li>
-                            <li><span>电话号码：<strong style="color:#F00">* </strong></span><input type="text": value="BuyerPhone"/></li>
-                            <li><span>联系QQ：<strong style="color:#F00">* </strong></span><input type="text": value="BuyerQQ"/></li>
-                            <li><span>交易暗码：<strong style="color:#F00">* </strong></span><input type="text" :value="Signal"/></li>
-                            <li><span>推广码：<strong style="color:#F00">&nbsp; &nbsp; </strong></span><input type="text":value="PromoNum" /></li>
-                        </ul>`;
+            <li class="buyNum">
+                <span>购买数量：<strong style="color:#F00">* </strong></span><input type="hidden"/>{{BuyNum}}
+                <p class="txcpdd_default"> (可购1件)</p>
+            </li>
+            <li>
+                <span>交易方式：<strong style="color:#F00">* </strong></span>
+                <input type="radio" name="goodsPurchaseBea" value="face" checked="checked" />&nbsp; 当面交易
+                <div style="border:solid red 1px; background:#f4faff;padding:5px;line-height:20px; margin-left:95px;color: red">
+                    请拍单后前往16线行会莎兰处进行交易
+                </div>
+            </li>
+            <li><span>所在区服：<strong style="color:#F00">* </strong></span>{{GameName}}/{{GroupName}}/{{ServerName}}</li>
+            <li><span>电话号码：<strong style="color:#F00">* </strong></span><input type="text": value="BuyerPhone"/></li>
+            <li><span>联系QQ：<strong style="color:#F00">* </strong></span><input type="text": value="BuyerQQ"/></li>
+            <li><span>交易暗码：<strong style="color:#F00">* </strong></span><input type="text" :value="Signal"/></li>
+            <li><span>推广码：<strong style="color:#F00">&nbsp; &nbsp; </strong></span><input type="text":value="PromoNum" /></li>
+        </ul>`;
 
         //数据
         var data = {
