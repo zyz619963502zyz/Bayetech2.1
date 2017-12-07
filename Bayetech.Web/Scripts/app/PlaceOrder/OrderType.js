caeedd2d77fa7c4c3468a69f907af9b666034800
zhaoyz5 <619963502@qq.com>
2017-12-07 09:41:02 +0800

    提交填写订单模块

--- Bayetech.Web/Scripts/app/PlaceOrder/OrderType.js
+++ Bayetech.Web/Scripts/app/PlaceOrder/OrderType.js
@@ -5,57 +5,60 @@ define(jsconfig.baseArr, function (Vue, $, common) {
     //var _GetGoodInfo = "";
 
     //模板
-    var goldorderHtml = `
-        <ul id="BuyGold" v-if="GoodTypeId==1"><!--金币-->
-            <li class="buyNum">
-                <span>购买数量：<strong style="color:#F00">* </strong></span><input type="hidden"/>{{BuyNum}}
-                <p class="txcpdd_default"> (可购1件)</p>
-            </li>
-            <li>
-                <span>交易方式：<strong style="color:#F00">* </strong></span>
-                <input type="radio" name="goodsPurchaseBea" value="face" checked="checked" />&nbsp; 当面交易
-                <div style="border:solid red 1px; background:#f4faff;padding:5px;line-height:20px; margin-left:95px;color: red">
-                    请拍单后前往16线行会莎兰处进行交易
-                </div>
-            </li>
-            <li><span>所在区服：<strong style="color:#F00">* </strong></span>{{GameName}}/{{GroupName}}/{{ServerName}}</li>
-            <li><span>电话号码：<strong style="color:#F00">* </strong></span><input type="text":value="BuyerPhone"/></li>
-            <li><span>联系QQ：<strong style="color:#F00">* </strong></span><input type="text":value="BuyerQQ"/></li>
-            <li><span>交易暗码：<strong style="color:#F00">* </strong></span><input type="text" :value="Signal"/></li>
-            <li><span>推广码：<strong style="color:#F00">&nbsp; &nbsp; </strong></span><input type="text":value="PromoNum" /></li>
-        </ul>
-
-        <ul id="BuyAccount" v-else-if="GoodTypeId==3"><!--账号-->
-            <li><span>购买数量：<strong style="color:#F00">* </strong></span><input type="text":value="BuyNum" readonly="readonly" /> (可购1件) </li>
-            <li><span>收货方式：<strong style="color:#F00">* </strong></span> {{GoodTypeName}}交易</li>
-            <li><span>所在区服：<strong style="color:#F00">* </strong></span>{{GameName}}/{{GroupName}}/{{ServerName}}</li>
-            <li><span>电话号码：<strong style="color:#F00">* </strong></span><input type="text":value="BuyerPhone" /></li>
-            <li><span>联系QQ：<strong style="color:#F00">* </strong></span><input type="text":value="BuyerQQ"/></li>
-            <li><span>推广码：<strong style="color:#F00">&nbsp; &nbsp; </strong></span><input type="text":value="PromoNum" /></li>
-        </ul>
-
-        <ul v-else-if="GoodTypeId==4"><!--点券-->
-            <li class ="buyNum">
-                    <span>购买数量：<strong style="color:#F00">* </strong></span><input type="text":value="BuyNum"><p class ="zq">&nbsp; </p>
+    var goldorderHtml=`
+          <div class ="txcpdd">
+            <h1>填写商品订单</h1>
+            <ul id="BuyGold" v-if="GoodTypeId==1"><!--金币-->
+                <li class="buyNum">
+                    <span>购买数量：<strong style="color:#F00">* </strong></span><input type="hidden"/>{{BuyNum}}
+                    <p class="txcpdd_default"> (可购1件)</p>
                 </li>
                 <li>
-                    <span>收货方式：<strong style="color:#F00">* </strong></span> 邮寄 交易
+                    <span>交易方式：<strong style="color:#F00">* </strong></span>
+                    <input type="radio" name="goodsPurchaseBea" value="face" checked="checked" />&nbsp; 当面交易
+                    <div style="border:solid red 1px; background:#f4faff;padding:5px;line-height:20px; margin-left:95px;color: red">
+                        请拍单后前往16线行会莎兰处进行交易
+                    </div>
                 </li>
                 <li><span>所在区服：<strong style="color:#F00">* </strong></span>{{GameName}}/{{GroupName}}/{{ServerName}}</li>
-                <li style="position: relative;overflow: visible;">
-                    <span>游戏账号：<strong style="color:#F00">* </strong></span><input type="text" :value="GameAccount">
-                    <div class ="fed-toolstip" id="tipFrame" style="left: 230px;position: absolute;display:none">
-                        <div class ="fed-tipcon">
-                            <div>如有人提供游戏账号让您填写，以做任务或刷信用为由，均为骗子，请勿受骗！</div>
-                            <a class ="fed-tipsclose" id="tipClose">×</a>
+                <li><span>电话号码：<strong style="color:#F00">* </strong></span><input type="text":value="BuyerPhone"/></li>
+                <li><span>联系QQ：<strong style="color:#F00">* </strong></span><input type="text":value="BuyerQQ"/></li>
+                <li><span>交易暗码：<strong style="color:#F00">* </strong></span><input type="text" :value="Signal"/></li>
+                <li><span>推广码：<strong style="color:#F00">&nbsp; &nbsp; </strong></span><input type="text":value="PromoNum" /></li>
+            </ul>
+
+            <ul id="BuyAccount" v-else-if="GoodTypeId==3"><!--账号-->
+                <li><span>购买数量：<strong style="color:#F00">* </strong></span><input type="text":value="BuyNum" readonly="readonly" /> (可购1件) </li>
+                <li><span>收货方式：<strong style="color:#F00">* </strong></span> {{GoodTypeName}}交易</li>
+                <li><span>所在区服：<strong style="color:#F00">* </strong></span>{{GameName}}/{{GroupName}}/{{ServerName}}</li>
+                <li><span>电话号码：<strong style="color:#F00">* </strong></span><input type="text":value="BuyerPhone" /></li>
+                <li><span>联系QQ：<strong style="color:#F00">* </strong></span><input type="text":value="BuyerQQ"/></li>
+                <li><span>推广码：<strong style="color:#F00">&nbsp; &nbsp; </strong></span><input type="text":value="PromoNum" /></li>
+            </ul>
+
+            <ul v-else-if="GoodTypeId==4"><!--点券-->
+                <li class ="buyNum">
+                        <span>购买数量：<strong style="color:#F00">* </strong></span><input type="text":value="BuyNum"><p class ="zq">&nbsp; </p>
+                    </li>
+                    <li>
+                        <span>收货方式：<strong style="color:#F00">* </strong></span> 邮寄 交易
+                    </li>
+                    <li><span>所在区服：<strong style="color:#F00">* </strong></span>{{GameName}}/{{GroupName}}/{{ServerName}}</li>
+                    <li style="position: relative;overflow: visible;">
+                        <span>游戏账号：<strong style="color:#F00">* </strong></span><input type="text" :value="GameAccount">
+                        <div class ="fed-toolstip" id="tipFrame" style="left: 230px;position: absolute;display:none">
+                            <div class ="fed-tipcon">
+                                <div>如有人提供游戏账号让您填写，以做任务或刷信用为由，均为骗子，请勿受骗！</div>
+                                <a class ="fed-tipsclose" id="tipClose">×</a>
+                            </div>
                         </div>
-                    </div>
-            </li>
-            <li><span>确认游戏账号：<strong style="color:#F00">* </strong></span><input type="text":value="GameAccountAgain"></li>
-            <li><span>电话号码：<strong style="color:#F00">* </strong></span><input type="text":value="BuyerPhone"></li>
-            <li><span>联系QQ：<strong style="color:#F00">* </strong></span><input type="text":value="BuyerQQ"></li>
-            <li><span>推广码：<strong style="color:#F00">&nbsp; &nbsp; </strong></span><input type="text":value="PromoNum"></li>
-         </ul>`;
+                </li>
+                <li><span>确认游戏账号：<strong style="color:#F00">* </strong></span><input type="text":value="GameAccountAgain"></li>
+                <li><span>电话号码：<strong style="color:#F00">* </strong></span><input type="text":value="BuyerPhone"></li>
+                <li><span>联系QQ：<strong style="color:#F00">* </strong></span><input type="text":value="BuyerQQ"></li>
+                <li><span>推广码：<strong style="color:#F00">&nbsp; &nbsp; </strong></span><input type="text":value="PromoNum"></li>
+             </ul>
+        </div>`;
 
         //数据
         var data={//填写的数据
