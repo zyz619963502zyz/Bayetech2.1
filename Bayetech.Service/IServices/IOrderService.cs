b968ec130ace1d42e91d26a356512b8af56e5e49
zhaoyz5 <619963502@qq.com>
2017-12-14 17:46:47 +0800

    提交获取订单信息

--- Bayetech.Service/IServices/IOrderService.cs
+++ Bayetech.Service/IServices/IOrderService.cs
@@ -1,10 +1,13 @@
 ﻿using Bayetech.Core.Entity;
 using Newtonsoft.Json.Linq;
 
-namespace Bayetech.Service.Services
+namespace Bayetech.Service.IServices
 {
     internal interface IOrderService
     {
         JObject CreatOrder(MallOrder order);
+
+        JObject GetOrderInfo(vw_MallOrderInfo order);
     }
+
 }
