3e8e441efae98eb1c649d960699593cad18551e4
dongwj5 <dongwj5@SHCYF58T>
2017-12-14 13:29:00 +0800

    完成个人中心搭建

--- Bayetech.Web/Scripts/app/UserCenter/Router.js
+++ Bayetech.Web/Scripts/app/UserCenter/Router.js
@@ -1,17 +1,26 @@
 ﻿//个人中心路由配置
-define([], function () {
-    var data={};
-    var components={
-        name: "ToSeller",
-        template: html,
-        data: function () {
-            return data;
-        },
-        created: function () {
+define(['Scripts/app/UserCenter/Buyer/ToBuy',
+    'Scripts/app/UserCenter/Buyer/MyOrders',
+    'Scripts/app/UserCenter/Buyer/MyDlOrder',
+    'Scripts/app/UserCenter/Buyer/MyDlRequire',
+    'Scripts/app/UserCenter/Buyer/BuyerDlOrder',
+    'Scripts/app/UserCenter/Seller/ToSeller',
+    'Scripts/app/UserCenter/Seller/OrdersManage',
+    'Scripts/app/UserCenter/Seller/GoodsManage',
+    'Scripts/app/UserCenter/Seller/DlPackage',
+    'Scripts/app/UserCenter/Seller/MyAcceptDlRequire',
+    'Scripts/app/UserCenter/AccountSettings/ChangePassword',
+    'Scripts/app/UserCenter/AccountSettings/MyPaymentPassword',
+    'Scripts/app/UserCenter/AccountSettings/PhoneBinding',
+    'Scripts/app/UserCenter/AccountSettings/Certification',
+    'Scripts/app/UserCenter/AccountSettings/ValidateLogin',], function () {
+    var routes=[];
+    for (var i=0; i<arguments.length; i++) {
+        routes.push({
+            path: '/'+arguments[i].name,
+            component: arguments[i]
+        })
+    }
 
-        },
-        methods: {
-        }
-    };
-    return components;
+    return { routes: routes };
 });
--- Bayetech.Web/Scripts/app/UserCenter/index.js
+++ Bayetech.Web/Scripts/app/UserCenter/index.js
@@ -1,25 +1,16 @@
-﻿var requireArr=['vue', 'jquery', 'common', 'VueRouter', 'v-header', 'v-footer', 'v-nav', 'v-search', 'v-menu',
-    '../Scripts/app/UserCenter/Buyer/ToBuy',
-    '../Scripts/app/UserCenter/Buyer/MyOrders',
-    '../Scripts/app/UserCenter/Buyer/MyDlOrder',
-    '../Scripts/app/UserCenter/Buyer/MyDlRequire',
-    '../Scripts/app/UserCenter/Buyer/BuyerDlOrder',
-
-
-
-];
+﻿var UserCenterRequireArr=['vue', 'jquery', 'common', 'VueRouter', '../Scripts/app/UserCenter/router', 'v-header', 'v-footer', 'v-nav', 'v-search', 'v-menu', ];
 
 //个人中心主模块
-require(requireArr, function () {
+require(UserCenterRequireArr, function () {
     //api
     debugger;
     var Vue=arguments[0];
     var $=arguments[1];
     var common=arguments[2];
     var VueRouter=arguments[3];
-    var ToBuy=arguments[9];
+    var router=arguments[4];
     var components={};
-    for (var i=3; i<arguments.length; i++){
+    for (var i=4; i<arguments.length; i++){
         components[arguments[i].name]=arguments[i];
     }
 
@@ -36,53 +27,30 @@ require(requireArr, function () {
                     { Id: 0, Title: "买家代练订单", Path: "/BuyerDlOrder" }],
             }, {
                 Title: "我是卖家",
-                Items: [{ Id: 0, Title: "我要卖", Path: "/About" },
-                    { Id: 0, Title: "我的订单管理", Path: "/Home" },
-                    { Id: 0, Title: "出售中的商品", Path: "/About" },//合并成商品管理
-                    { Id: 0, Title: "仓库中的商品", Path: "/About" },//合并成商品管理
+                Items: [{ Id: 0, Title: "我要卖", Path: "/ToSeller" },
+                    { Id: 0, Title: "我的订单管理", Path: "/OrdersManage" },
+                    { Id: 0, Title: "出售中的商品", Path: "/GoodsManage" },//合并成商品管理
+                    { Id: 0, Title: "仓库中的商品", Path: "/GoodsManage" },//合并成商品管理
                     //{ Id: 0, Title: "我的出货订单", Path: "/About" },合并到订单管理
-                    { Id: 0, Title: "我发布的代练套餐", Path: "/About" },
-                    { Id: 0, Title: "我接的代练需求", Path: "/About" },
+                    { Id: 0, Title: "我发布的代练套餐", Path: "/DlPackage" },
+                    { Id: 0, Title: "我接的代练需求", Path: "/MyAcceptDlRequire" },
                 ],
 
             }, {
                 Title: "账号设置",
-                Items: [{ Id: 0, Title: "修改登录密码", Path: "/About" },
-                    { Id: 0, Title: "我的支付密码", Path: "/Home" },
-                    { Id: 0, Title: "手机绑定", Path: "/About" },
-                    { Id: 0, Title: "实名认证", Path: "/About" },
-                    { Id: 0, Title: "登录验证", Path: "/About" }],
+                Items: [{ Id: 0, Title: "修改登录密码", Path: "/ChangePassword" },
+                    { Id: 0, Title: "我的支付密码", Path: "/MyPaymentPassword" },
+                    { Id: 0, Title: "手机绑定", Path: "/PhoneBinding" },
+                    { Id: 0, Title: "实名认证", Path: "/Certification" },
+                    { Id: 0, Title: "登录验证", Path: "/ValidateLogin" }],
             }]
             
         },
     }
-    var Home=Vue.extend({
-        template: '<p>#HomeMoudle</P>',
-    })
-
-    var About=Vue.extend({
-        template: '<p>#AboutMoudle</p>'
-    })
 
     Vue.use(VueRouter)
     /* 创建路由器  */
-    var router=new VueRouter({
-        routes: [
-            { 
-                path: '/Home',
-                component: Home,
-            },
-            { 
-                path: '/About',
-                component: About
-            },
-            {
-                path: '/ToBuy',
-                component: ToBuy
-            },
-        ]
-    });
-
+    var router=new VueRouter(router);
 
     var vm = new Vue({
         el: '#app',
