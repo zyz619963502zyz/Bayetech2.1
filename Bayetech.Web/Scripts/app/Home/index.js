12f235cfc12d08d96ffdec3dc7dd54dee7c2b7d7
liuzy34 <liuzy34@SHCYF25T>
2017-12-19 16:33:29 +0800

    提交

--- Bayetech.Web/Scripts/app/Home/index.js
+++ Bayetech.Web/Scripts/app/Home/index.js
@@ -2,7 +2,8 @@
 require(['vue', 'jquery', 'common', 'bootstrap', 'v-header', 'v-nav', 'v-search', 'index-notice', 'index-consult', 'index-convenience'
     , 'index-hotgamelist', 'index-banner', 'index-tabslist', 'v-tab', "index-gameranking", 'index-adv', 'index-mgamelist'
     , 'v-partner', 'index-slidebox', 'nav-top', 'css!../Content/bootstrap/font-awesome.min', 'css!../Content/common', 'css!../Content/homepage'],
-	function (Vue, $, common, bootstrap, header, nav, search, notice, consult, convenience, hotgamelist, banner, tabsListbox, tab, gameranking, adv, mgamelist, partner, slidebox,navt) {
+	function (Vue, $, common, bootstrap, header, nav, search, notice, consult,
+		convenience, hotgamelist, banner, tabsListbox, tab, gameranking, adv, mgamelist, partner, slidebox, navt) {
      
 	    debugger;
     new Vue({
