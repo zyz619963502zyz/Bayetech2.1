f1339ea8d6c1fc3700541cc4e831069a555066e7
jie <jie@fhey.me>
2018-01-19 15:20:23 +0800

    改变css引用顺序

--- Bayetech.Web/Scripts/jsconfig.js
+++ Bayetech.Web/Scripts/jsconfig.js
@@ -86,7 +86,7 @@ var jsconfig = {
     },
     shim: {
         'bootstrap': {
-            deps: ['jquery', , 'css!../Content/bootstrap/bootstrap.min'],
+            deps: ['jquery'],
             exports: 'aaaa'
         },
         'bootstrapValidator': {
@@ -102,7 +102,7 @@ var jsconfig = {
         	exports: 'datepicker'
         },
         'swiper': {
-            deps: ['jquery', 'bootstrap', 'css!../Content/swiper/swiper.min'],
+            deps: ['jquery', 'bootstrap'],
             exports: 'Swiper',
         }
     }
