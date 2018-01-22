d4cfb33718e6c4e256d316dff0c76a98d2de6614
zhaoyz5 <619963502@qq.com>
2018-01-22 14:07:39 +0800

    提交表变动

--- Bayetech.Web/Scripts/app/UserCenter/index.js
+++ Bayetech.Web/Scripts/app/UserCenter/index.js
@@ -20,13 +20,13 @@ require(UserCenterRequireArr, function () {
                 Title: "我是买家",
                 Items: [{ Id: 0, Title: "我要买", Path: "../Game/List.html", Type: "url" },
                     { Id: "MyOrders", Title: "我购买的订单", Path: "/MyOrders", Params: {flag:"All"}},
-                    { Id: "MyDlOrders", Title: "我要代练", Path: "/MyOrders", Params: { flag: "DL" } },//BuyerDlOrder代练订单和购买订单类似。
-                    { Id: 0, Title: "我的代练订单", Path: "/MyDlRequire" }],
+                    { Id: "MyDlRequire", Title: "我要代练", Path: "/MyDlRequire", Params: { flag: "DL" } },//BuyerDlOrder代练订单和购买订单类似。
+                    { Id: 0, Title: "我的代练订单", Path: "/MyDlOrder" }],
             }, {
                 Title: "我是卖家",
                 Items: [{ Id: 0, Title: "我要卖", Path: "../Good/Publish.html", Type: "url" },
                     { Id: 0, Title: "我的订单管理", Path: "/MyOrders", Params: {flag:"Sales"} },
-                    { Id: 0, Title: "商品管理", Path: "/GoodManage" },//合并成商品管理
+                    { Id: 0, Title: "我的商品管理", Path: "/GoodManage" },//合并成商品管理
                     { Id: 0, Title: "我发布的代练", Path: "/DlPackage" },
                 ],
 
@@ -37,8 +37,7 @@ require(UserCenterRequireArr, function () {
                     { Id: 0, Title: "手机绑定", Path: "/PhoneBinding" },
                     { Id: 0, Title: "实名认证", Path: "/Certification" },
                     { Id: 0, Title: "登录验证", Path: "/ValidateLogin" }],
-            }]
-            
+            }]  
         },
     } 
 
