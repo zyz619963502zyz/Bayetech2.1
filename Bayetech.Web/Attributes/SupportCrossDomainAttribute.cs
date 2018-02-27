76369c676d2a1bc254f3b51b2b5b5f46237e8a15
zhaoyz5 <619963502@qq.com>
2018-02-27 15:22:30 +0800

    提交

---
+++ Bayetech.Web/Attributes/SupportCrossDomainAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Http.Filters;
+
+namespace Bayetech.Web
+{
+    /// <summary>
+    /// 跨域过滤器
+    /// </summary>
+    public class SupportCrossDomainAttribute:ActionFilterAttribute
+    { 
+        public bool AllowCredentials { get; set; }
+        public string Origin { get; set; }
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            var resHeader = actionExecutedContext.Response.Headers;
+
+            if (AllowCredentials)
+            {
+                resHeader.Add("Access-Control-Allow-Credentials","true");
+            }
+
+            resHeader.Add("Access-Control-Allow-Origin", Origin ?? "*");
+
+            base.OnActionExecuted(actionExecutedContext);
+        }
+    }
+}
