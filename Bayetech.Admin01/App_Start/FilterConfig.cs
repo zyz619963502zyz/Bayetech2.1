b7521456184e4b36e49168af62d52220d507d1f2
zgy <78695@DESKTOP-28FL8RR>
2018-04-16 22:37:00 +0800

    zgy

---
+++ Bayetech.Admin01/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Bayetech.Admin01
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Bayetech.Admin01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bayetech.Admin01.Startup))]
+namespace Bayetech.Admin01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
