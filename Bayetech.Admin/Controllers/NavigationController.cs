7905d7fe8e83d0108a0ec59b2121a072c887d26c
zgy <78695@DESKTOP-28FL8RR>
2018-04-25 17:20:20 +0800

    zgy

--- Bayetech.Admin/Controllers/NavigationController.cs
+++ Bayetech.Admin/Controllers/NavigationController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +10,10 @@ namespace Bayetech.Admin.Controllers
 {
     public class NavigationController : BaseController
     {
+        [HttpGet]
+        public JObject CheckAccount()
+        {
+            return null;
+        }
     }
 }
