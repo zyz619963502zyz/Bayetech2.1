7397c60833bfe0949a5d643cdadd4226c1990d9c
microliuzy <kakaliu_001@163.com>
2018-04-25 17:01:39 +0800

    提交

--- Bayetech.Web/Scripts/app/Game/index.js
+++ Bayetech.Web/Scripts/app/Game/index.js
@@ -1,6 +1,6 @@
 ﻿//模块之间的操作
-require(['vue', 'jquery', 'common', 'bootstrap', 'v-header', 'v-nav', 'v-search', 'game-list',"v-footer"],
-	function (Vue, $, common, bootstrap, header, nav, search, GameList,footer) {
+require(['vue', 'jquery', 'common', 'bootstrap', 'v-header', 'v-nav','nav-top', 'v-search', 'game-list',"v-footer"],
+	function (Vue, $, common, bootstrap, header, nav, navt, search, GameList,footer) {
 	    new Vue({
 	        el: '#app',
 	        data() {
@@ -9,6 +9,7 @@ require(['vue', 'jquery', 'common', 'bootstrap', 'v-header', 'v-nav', 'v-search'
 	        components: {
 	            "v-header": header,
 	            "v-nav": nav,
+                "nav-top":navt,
 	            "v-search": search,
 	            "GameList": GameList,
                 "v-footer":footer
