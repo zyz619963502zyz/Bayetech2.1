c4f8b059528807fa084f814a5af95b842a45ee2c
zhaoyz5 <619963502@qq.com>
2018-05-03 19:14:40 +0800

    提交所有的代码更改

---
+++ Bayetech.Admin/Scripts/app/GoodCheck.es5.es5.js
@@ -0,0 +1,22 @@
+﻿"use strict";
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { "default": obj };
+}
+
+var _commonJs = require('./common.js');
+
+var _commonJs2 = _interopRequireDefault(_commonJs);
+
+var _url = "/api/Test/GoodCheck";
+var aa = 1;
+var aaaa = 2;
+
+var aaa = _commonJs2["default"];
+//commonCompnent.postWebJson(_url,null,function(){
+
+//});
+
+//当前执行的函数
+(function () {})();
+
