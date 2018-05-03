7df7f0941aafd97f510a195d22ccdfc777cb1df6
zhaoyz5 <619963502@qq.com>
2018-05-03 23:25:01 +0800

    提交代码

--- Bayetech.Admin/Controllers/BaseController.cs
+++ Bayetech.Admin/Controllers/BaseController.cs
@@ -1,10 +1,6 @@
 ﻿using Newtonsoft.Json.Linq;
 using Spring.Context;
 using Spring.Context.Support;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Http;
 
 namespace Bayetech.Admin.Controllers
---
+++ Bayetech.Admin/Scripts/app/GoodCheck.es5.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+
+//import commonCompnent from './common.js';暂时无法使用ES6写法，日后再说
+
+//var _url = "/api/Test/GoodCheck";
+//const aa = 1;
+//let aaaa = 2;
+
+//var aaa = commonCompnent;
+////commonCompnent.postWebJson(_url,null,function(){
+
+////});
+
