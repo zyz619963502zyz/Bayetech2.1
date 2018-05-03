c5e9b1782f922df642f80e4b4a42fd58c211feb0
zhaoyz5 <619963502@qq.com>
2018-05-03 20:02:02 +0800

    提交当前改动

--- Bayetech.Admin/Scripts/app/GoodCheck.es5.es5.js
+++ Bayetech.Admin/Scripts/app/GoodCheck.es5.es5.js
@@ -1,22 +1,48 @@
-﻿"use strict";
+﻿//import commonCompnent from './common.js';
 
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : { "default": obj };
-}
+//var _url = "/api/Test/GoodCheck";
+//const aa = 1;
+//let aaaa = 2;
 
-var _commonJs = require('./common.js');
-
-var _commonJs2 = _interopRequireDefault(_commonJs);
-
-var _url = "/api/Test/GoodCheck";
-var aa = 1;
-var aaaa = 2;
-
-var aaa = _commonJs2["default"];
+//var aaa = commonCompnent;
 //commonCompnent.postWebJson(_url,null,function(){
-
 //});
 
 //当前执行的函数
-(function () {})();
+"use strict";
+
+(function () {
+    var vmData = {
+        BaseUrl: common.GetBaseUrl() + "Good/GoodInfo.html?GoodNo=",
+        BaseTarget: "_blank",
+        keyword: "",
+        ListObj: [{
+            GoodNo: "",
+            GoodFirstPicture: "",
+            aurl: "",
+            GoodTitle: "",
+            GroupName: "",
+            ServerName: "",
+            GoodPrice: ""
+        }],
+        SearchParam: {
+            Param: eval('(' + localStorage.SearchParam + ')'),
+            Pagination: { //分页对象
+                rows: 10, //每页行数，
+                page: 1, //当前页码
+                order: "GoodNo", //排序字段
+                sord: "asc", //排序类型
+                records: 10, //总记录数
+                total: 10 //总页数。
+            }
+        }
+    };
+
+    var vm = new vue({
+        el: "#abc",
+        data: vmData,
+        created: function created() {},
+        methods: {}
+    });
+})();
 
--- Bayetech.Admin/Scripts/app/GoodCheck.js
+++ Bayetech.Admin/Scripts/app/GoodCheck.js
@@ -1,15 +1,64 @@
-﻿import commonCompnent from './common.js';
+﻿//import commonCompnent from './common.js';
 
-var _url = "/api/Test/GoodCheck";
-const aa = 1;
-let aaaa = 2;
+//var _url = "/api/Test/GoodCheck";
+//const aa = 1;
+//let aaaa = 2;
 
-var aaa = commonCompnent;
+//var aaa = commonCompnent;
 //commonCompnent.postWebJson(_url,null,function(){
 //});
 
 
 //当前执行的函数
-(function(){
+(function () {
+
+    var vmData = {
+        BaseUrl: common.GetBaseUrl()+"Good/GoodInfo.html?GoodNo=",
+        BaseTarget: "_blank",
+        keyword: "",
+        GoodInfoArray:[],
+        ListObj: [
+            {
+                GoodNo: "",
+                GoodFirstPicture: "",
+                aurl: "",
+                GoodTitle: "",
+                GroupName: "",
+                ServerName: "",
+                GoodPrice: ""
+            }
+        ],
+        SearchParam: {
+            Param: eval('('+localStorage.SearchParam+')'),
+            Pagination: {//分页对象
+                rows: 10,//每页行数，
+                page: 1,//当前页码
+                order: "GoodNo",//排序字段
+                sord: "asc",//排序类型
+                records: 10,//总记录数
+                total: 10//总页数。
+            }
+        },
+     };
+
+    var vm = new Vue({
+        el: "#CommForm",
+        data: vmData,
+        created(){
+
+        },
+        methods: {
+             findList() {//获取商品的简要列表
+                var self=this;
+                common.postWebJson(GoodListUrl, self.SearchParam, function (data) {
+                    if (data.result) {
+                        self.GoodInfoArray=data.content.datas;
+                    }
+                });
+            }
+        }
+    });
 
 })();
+
+
