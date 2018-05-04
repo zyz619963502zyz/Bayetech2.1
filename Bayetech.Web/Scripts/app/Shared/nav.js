941fd0aea1bef1092bf2d7e750d20d9544fb2fb7
microliuzy <kakaliu_001@163.com>
2018-05-04 17:14:11 +0800

    修修补补

--- Bayetech.Web/Scripts/app/Shared/nav.js
+++ Bayetech.Web/Scripts/app/Shared/nav.js
@@ -2,9 +2,9 @@
 define([], function () {
     var html = `<div class="nav-box"><ul class="nav nav-pills nav-justified" role="tablist">
                 <li class="active"><a href="#home0">首页</a></li>
-                <li><a href="../../Game/List.html" target="_blank">金币交易</a></li>
-                <li><a href="../../Game/List.html" target="_blank">账号交易</a></li>
-                <li><a href="../../Game/List.html" target="_blank">装备交易</a></li>
+                <li><a href="../Game/List.html" target="_blank">金币交易</a></li>
+                <li><a href="../Game/List.html" target="_blank">账号交易</a></li>
+                <li><a href="../Game/List.html" target="_blank">装备交易</a></li>
                 <li><a href="#">游戏代练</a></li>
             </ul></div>`;
 
