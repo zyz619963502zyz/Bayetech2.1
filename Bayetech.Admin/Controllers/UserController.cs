809f9ef7787b2cf1eb8a5584c9d31e91a5303309
78695 <78695@DESKTOP-28FL8RR>
2018-05-08 16:03:04 +0800

    zgy

--- Bayetech.Admin/Controllers/UserController.cs
+++ Bayetech.Admin/Controllers/UserController.cs
@@ -14,41 +14,19 @@ namespace Bayetech.Admin.Controllers
 {
     public class UserController : BaseController
     {
-        BaseService<Admin_Sys_Users> UsersService = new BaseService<Admin_Sys_Users>();
 
-        /// <summary>
-        /// 获取一个列表，供给Grid使用
-        /// </summary>
-        /// <param name="sortOrder"></param>
-        /// <param name="limit"></param>
-        /// <param name="offset"></param>
-        /// <param name="searchTxt"></param>
-        /// <returns></returns>
+        IUserService userService = ctx.GetObject("UserService") as IUserService;
         [HttpGet]
-        public string GetList(string sortOrder, int limit, int offset, string searchTxt)
+        public bool AdminLogion(string userName,string passWord)
         {
-            RepositoryBase repository = new RepositoryBase();
-            IQueryable<Admin_Sys_Users> users = repository.IQueryable<Admin_Sys_Users>() ;
-
-            var total = users.Count();
-            if (limit > 1000) limit = 100;
-            if (limit > total)
+            try
             {
-                limit = total;
-                offset = 0;
+                return userService.GetVerificationLogion(userName,passWord);
             }
-            else
+            catch (Exception ex)
             {
-                if (offset >= total - limit) offset = total - limit;
+                throw new Exception(ex.Message);
             }
-            users = (string.IsNullOrEmpty(sortOrder) || sortOrder == "asc") ? users.OrderBy(a => a.KeyId) : users.OrderByDescending(a => a.KeyId);
-            if (!string.IsNullOrEmpty(searchTxt))
-            {
-                users = users.Where(c => c.UserName.Contains(searchTxt) || c.TrueName.Contains(searchTxt));
-            }
-            var ret = users.Select(a => new { a.KeyId, a.UserName, a.TrueName, a.DepartmentId, a.Mobile, a.Remark, a.IsAdmin, a.IsDisabled }).Skip(offset).Take(limit).ToList();
-
-            return JSONhelper.GetJsonforBootstrapTable(total, ret);
         }
     }
 }
--- Bayetech.Service/IServices/INavigationService.cs
+++ Bayetech.Service/IServices/INavigationService.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bayetech.Service.IServices
+namespace Bayetech.Service
 {
     public interface INavigationService
     {
