1bad509443c0204b00e027f4071cc9d5824edfe7
78695 <78695@DESKTOP-28FL8RR>
2018-05-10 12:29:48 +0800

    zgy

---
+++ Bayetech.Service/Services/AdminManageService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Bayetech.Service
+{
+    public class AdminManageService : IAdminManageService
+    {
+        public JObject GetUserList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
