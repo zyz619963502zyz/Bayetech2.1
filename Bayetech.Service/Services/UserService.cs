182c60f0700bc19b1ae10cd9b7f52e86cc551ddc
78695 <78695@DESKTOP-28FL8RR>
2018-05-10 11:08:42 +0800

    zgy

--- Bayetech.Service/Services/UserService.cs
+++ Bayetech.Service/Services/UserService.cs
@@ -6,12 +6,60 @@ using System;
 using Bayetech.Core;
 using Bayetech.Service.IServices;
 using System.Web;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace Bayetech.Service
 {
     public class UserService : IUserService
     {
+        public bool CheckAccount(string account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public JObject CheckLogin(JObject json)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CreatAccount(JObject json)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(object keyValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User FindEntity(object keyValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<User> FindList(Expression<Func<User, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> FindList(Expression<Func<User, bool>> predicate, string order = "")
+        {
+            throw new NotImplementedException();
+        }
+
         public bool GetVerificationLogion(string userName, string passWord)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(User entity)
         {
             throw new NotImplementedException();
         }
