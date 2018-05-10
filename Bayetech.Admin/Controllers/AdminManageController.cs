dc1369af664392ea9af102c3fb11fcd56c7d2dce
78695 <78695@DESKTOP-28FL8RR>
2018-05-10 13:55:40 +0800

    zgy

---
+++ Bayetech.Admin/Controllers/AdminManageController.cs
@@ -0,0 +1,33 @@
+﻿using Bayetech.Service;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Bayetech.Admin.Controllers
+{
+    public class AdminManageController : BaseController
+    {
+        IAdminManageService adminManageService = ctx.GetObject("AdminManageService") as IAdminManageService;
+        /// <summary>
+        /// 获取管理员员工列表1
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public JObject GetList()
+        {
+            try
+            {
+                return adminManageService.GetUserList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
