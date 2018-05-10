bc99a20f92bad1975f3c9556140bc9cbe872e166
zhaoyz5 <619963502@qq.com>
2018-05-10 10:44:50 +0800

    提交还原

---
+++ Bayetech.Admin/Scripts/components/table.js
@@ -0,0 +1,25 @@
+﻿let BaseTable = `<template>
+    <table class="table table-bordered">
+        <thead>
+            <tr class="success">
+                <th class="text-center"  v-for="item in GoodTitleArray">{{item.value}}</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in GoodInfoArray">
+                <td>{{item.GoodNo}}</td>
+                <td>{{item.GameName}}</td>
+                <td>{{item.GoodTypeName}}</td>
+                <td>{{item.GoodKeyWord}}</td>
+                <td>{{item.GoodTitle}}</td>
+                <td class="text-center"><input type="button" class="btn btn-primary" value="审核商品" @click="StartCheck(item.GoodNo)"></td>
+            </tr>
+        </tbody>
+    </table>
+</template>`
+
+export default{
+    name:'BaseTable',
+    template:BaseTable,
+    props:['GoodTitleArray','GoodInfoArray','StartCheck']
+}
