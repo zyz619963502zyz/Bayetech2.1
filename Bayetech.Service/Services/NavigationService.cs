1b334ce26f5fafe0e33b389a0eb639072afb7f32
78695 <78695@DESKTOP-28FL8RR>
2018-05-17 17:58:23 +0800

    zgy

--- Bayetech.Service/Services/NavigationService.cs
+++ Bayetech.Service/Services/NavigationService.cs
@@ -9,11 +9,81 @@ using System.Collections.Concurrent;
 using Bayetech.Service.Model;
 using Bayetech.Core.Security.Json;
 using Newtonsoft.Json.Linq;
+using Bayetech.Core;
+using Newtonsoft.Json;
 
 namespace Bayetech.Service
 {
     public class NavigationService : BaseService<Admin_Sys_Navigations>, INavigationService
     {
+        public JObject AddNavigation(JObject json)
+        {
+            Admin_Sys_Navigations _admin_Sys_Navigations = (Admin_Sys_Navigations)JsonConvert.DeserializeObject(json.Last.Path, typeof(Admin_Sys_Navigations));
+            JObject result = new JObject();
+            if (string.IsNullOrEmpty(_admin_Sys_Navigations.NavTitle))
+            {
+                result.Add(ResultInfo.Result, JProperty.FromObject(false));
+                result.Add(ResultInfo.Content, JProperty.FromObject("菜单名称不能为空")); 
+            }
+            var _adminNavigation = repository.FindEntity<Admin_Sys_Navigations>(a=>a.NavTitle== _admin_Sys_Navigations.NavTitle);
+            if (_adminNavigation != null)
+            {
+                result.Add(ResultInfo.Result, JProperty.FromObject(false));
+                result.Add(ResultInfo.Content, JProperty.FromObject("菜单名称不能重复"));
+            }
+            else
+            {
+                if (_admin_Sys_Navigations.KeyId == 0)
+                {
+                    var add = repository.Insert<Admin_Sys_Navigations>(_admin_Sys_Navigations);
+                    if (add == 1)
+                    {
+                        result.Add(ResultInfo.Result, JProperty.FromObject(true));
+                        result.Add(ResultInfo.Content, JProperty.FromObject("操作成功"));
+                    }
+                    else
+                    {
+                        result.Add(ResultInfo.Result, JProperty.FromObject(false));
+                        result.Add(ResultInfo.Content, JProperty.FromObject("操作失败"));
+                    }
+                }
+                else
+                {
+                    var uopdate = repository.Update<Admin_Sys_Navigations>(_admin_Sys_Navigations);
+                    if (uopdate == 1)
+                    {
+                        result.Add(ResultInfo.Result, JProperty.FromObject(true));
+                        result.Add(ResultInfo.Content, JProperty.FromObject("修改成功"));
+                    }
+                    else
+                    {
+                        result.Add(ResultInfo.Result, JProperty.FromObject(false));
+                        result.Add(ResultInfo.Content, JProperty.FromObject("修改失败"));
+                    }
+
+                }
+            }
+            throw new NotImplementedException();
+        }
+
+        public JObject DeleteNavigation(JObject json)
+        {
+            Admin_Sys_Navigations _admin_Sys_Navigations = (Admin_Sys_Navigations)JsonConvert.DeserializeObject(json.Last.Path, typeof(Admin_Sys_Navigations));
+            var deleteNavigations = repository.Delete<Admin_Sys_Navigations>(_admin_Sys_Navigations);
+            JObject result = new JObject();
+            if (deleteNavigations == 1)
+            {
+                result.Add(ResultInfo.Result, JProperty.FromObject(true));
+                result.Add(ResultInfo.Content, JProperty.FromObject("删除成功"));
+            }
+            else
+            {
+                result.Add(ResultInfo.Result, JProperty.FromObject(false));
+                result.Add(ResultInfo.Content, JProperty.FromObject("删除失败"));
+            }
+            throw new NotImplementedException();
+        }
+
         /// <summary>
         /// 获取导航菜单列表
         /// </summary>
@@ -79,7 +149,20 @@ namespace Bayetech.Service
 
         public JObject GetList(string parentId)
         {
-            return null;
+
+            var adminNavigation = repository.IQueryable<Admin_Sys_Navigations>(a => a.ParentID == int.Parse(parentId));
+            JObject result = new JObject();
+            if (adminNavigation == null)
+            {
+                result.Add(ResultInfo.Result, JProperty.FromObject(false));
+                result.Add(ResultInfo.Content, JProperty.FromObject("没有找到匹配数据"));
+            }
+            else
+            {
+                result.Add(ResultInfo.Result, JProperty.FromObject(true));
+                result.Add(ResultInfo.Content, JProperty.FromObject(adminNavigation.ToList()));
+            }
+            return result;
         }
     }
 }
