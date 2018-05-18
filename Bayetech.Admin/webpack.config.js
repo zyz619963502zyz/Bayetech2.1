52f5b1591e81830c38892c62472e221ed95503f4
zhaoyz5 <619963502@qq.com>
2018-05-18 17:49:38 +0800

    提交权限JS文件

--- Bayetech.Admin/webpack.config.js
+++ Bayetech.Admin/webpack.config.js
@@ -6,7 +6,14 @@ const { VueLoaderPlugin } = require('vue-loader');//解决Make sure to include V
 
 module.exports = {
     entry:{
-        //TestES6: "./Scripts/TestES6.js",
+        //权限
+        Auth_CustomerManage:"./Scripts/Auth_CustomerManage",//注册客户管理
+        Auth_NavigationManage:"./Scripts/Auth_NavigationManage.js",//导航菜单
+        Auth_RolesManage:"./Scripts/Auth_RolesManage.js",//角色管理
+        Auth_DicsManage:"./Scripts/Auth_DicsManage.js",//数据字典
+        Auth_AdminManage: "./Scripts/Auth_AdminManage.js",//管理员设置
+        Auth_ButtonsManage:"./Scripts/Auth_ButtonsManage.js",//按钮管理
+        //业务
         GoodCheck: "./Scripts/app/GoodCheck.js"
     },
     output: {
