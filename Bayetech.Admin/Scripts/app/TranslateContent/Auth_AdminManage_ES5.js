ea926e029dc09783b34725d70f65d0e7d847475b
zhaoyz5 <619963502@qq.com>
2018-05-26 15:39:56 +0800

    提交代码

--- Bayetech.Admin/Scripts/app/TranslateContent/Auth_AdminManage_ES5.js
+++ Bayetech.Admin/Scripts/app/TranslateContent/Auth_AdminManage_ES5.js
@@ -63,12 +63,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 11);
+/******/ 	return __webpack_require__(__webpack_require__.s = 6);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 11:
+/***/ 6:
 /***/ (function(module, exports) {
 
 11111111111;
--- Bayetech.Admin/Scripts/app/TranslateContent/Auth_CustomerManage_ES5.js
+++ Bayetech.Admin/Scripts/app/TranslateContent/Auth_CustomerManage_ES5.js
@@ -63,12 +63,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 13);
+/******/ 	return __webpack_require__(__webpack_require__.s = 8);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 13:
+/***/ 8:
 /***/ (function(module, exports) {
 
 
