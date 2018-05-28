457eeb208aca8a414555896037b4f938b92dd1d5
zhaoyz5 <619963502@qq.com>
2018-05-28 18:29:31 +0800

    提交登录controller

--- Bayetech.Admin/Controllers/LoginController.cs
+++ Bayetech.Admin/Controllers/LoginController.cs
@@ -36,7 +36,7 @@ namespace Bayetech.Admin.Controller
                     Dictionary<string, string> param = new Dictionary<string, string>();
                     param.Add("id", "1");
                     param.Add("name", "wahaha");
-                    //Tuple<string, string> parameters = WebApiHelper.GetQueryString(parames);
+                    Tuple<string, string> parameters = WebApiHelper.GetQueryString(param);
                     //var product1 = WebApiHelper.Get<ProductResultMsg>("http://localhost:14826/api/product/getproduct", parameters.Item1, parameters.Item2, staffId);
                     //Product product = new Product() { Id = 1, Name = "安慕希", Count = 10, Price = 58.8 };
                     //var product2 = WebApiHelper.Post<ProductResultMsg>("http://localhost:14826/api/product/addProduct", JsonConvert.SerializeObject(product), staffId);
