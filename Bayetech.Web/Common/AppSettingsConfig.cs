f07425c3440b72cd4cf1a588e77f8b35c1cb0c99
jie <jie@fhey.me>
2018-06-13 08:10:15 +0800

    提交

---
+++ Bayetech.Web/Common/AppSettingsConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Configuration;
+using System.Runtime.CompilerServices;
+
+namespace Bayetech.Web
+{
+    public class AppSettingsConfig
+    {
+        public static string GetTokenApi
+        {
+            get
+            {
+                return AppSettingValue();
+            }
+        }
+
+        public static string StaffId
+        {
+            get
+            {
+                return AppSettingValue();
+            }
+        }
+
+        private static string AppSettingValue([CallerMemberName] string key = null)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+    }
+}
--- Bayetech.Web/Scripts/common.js
+++ Bayetech.Web/Scripts/common.js
@@ -1,83 +1,120 @@
 ﻿define("common", ['jquery'], function () {
     var common = {};
 
-    /**
-        * ajax封装
-        * url 发送请求的地址
-        * data 发送到服务器的数据，数组存储，如：{"date": new Date().getTime(), "state": 1}
-        * dataType 预期服务器返回的数据类型，常用的如：xml、html、json、text
-        * successfn 成功回调函数
-        * errorfn 失败回调函数
-        * asyncC:此为第五个参数传就是同步，不穿默认异步。
-    */
-    common.getWebJson = function (url, data, successfn, errorfn, asyncC, callLoading) {
-        if (!callLoading) {
+    //返回到特定的页面(改vue路由以后要变动)
+    common.GotoSpecialPage = function (data) {
+        if (data) {
+            switch (data.StatusCode) {
+                case 1:
+                    window.parent.location.href = "/Page/Login.html";
+                    break;
+                case 2:
+                    window.parent.location.href = "/Page/Error404.html";
+                    break;
+                default:
+                    break;
+            }
         }
-        //data = (data == null || data == "" || typeof (data) == "undefined") ? { "date": new Date().getTime() } : data;
-        $.ajax({
-            type: "get",
-            data: data,
-            url: url + "?time=" + new Date().getTime(),
-            dataType: "json",
-            global: false,
-            async: asyncC == undefined ? true : false,
-            success: function (d) {
-                successfn(d);
-            },
-            error: function (e) {
-                //e={"readyState":0,"status":0,"statusText":"error"}
-                //e={"readyState":4,"status":506,"statusText":"SessionTimeout"}
-                if (e.status === 506) {
-                    GetAlert("登录超时！");
-                    setTimeout("window.location ='" + window.appCtx['aist-sso-web'] + "/login?service=" + window.appCtx['ctm-web'] + "'", 2000);
-                } else {
-                    errorfn("status:" + e.status + "-" + e.statusText);
+    },
+
+        /**
+            * ajax封装
+            * url 发送请求的地址
+            * data 发送到服务器的数据，数组存储，如：{"date": new Date().getTime(), "state": 1}
+            * dataType 预期服务器返回的数据类型，常用的如：xml、html、json、text
+            * successfn 成功回调函数
+            * errorfn 失败回调函数
+            * asyncC:此为第五个参数传就是同步，不穿默认异步。
+        */
+        common.getWebJson = function (url, data, successfn, errorfn, asyncC, callLoading) {
+            var self = this;
+            if (!callLoading) {
+            }
+            //data = (data == null || data == "" || typeof (data) == "undefined") ? { "date": new Date().getTime() } : data;
+            $.ajax({
+                type: "get",
+                data: data,
+                url: url + "?time=" + new Date().getTime(),
+                headers: this.GetCookie("admin") == "" ? null : eval('(' + this.GetCookie("admin") + ')'),
+                dataType: "json",
+                global: false,
+                async: asyncC == undefined ? true : false,
+                success: function (d) {
+                    self.GotoSpecialPage(d);
+                    successfn(d);
+                },
+                error: function (e) {
+                    //e={"readyState":0,"status":0,"statusText":"error"}
+                    //e={"readyState":4,"status":506,"statusText":"SessionTimeout"}
+                    if (e.status === 506) {
+                        //GetAlert("登录超时！");
+                        setTimeout("window.location ='" + window.appCtx['aist-sso-web'] + "/login?service=" + window.appCtx['ctm-web'] + "'", 2000);
+                    } else {
+                        errorfn("status:" + e.status + "-" + e.statusText);
+                    }
+                }
+            });
+        },
+
+        /**
+           * ajax封装
+           * url 发送请求的地址
+           * data 发送到服务器的数据，数组存储，如：{"date": new Date().getTime(), "state": 1}
+           * dataType 预期服务器返回的数据类型，常用的如：xml、html、json、text
+           * successfn 成功回调函数
+           * errorfn 失败回调函数
+           * asyncC:此为第五个参数传就是同步，不穿默认异步。
+        */
+        common.postWebJson = function (url, data, successfn, errorfn, asyncC, callLoading) {
+            var self = this;
+            if (!callLoading) {
+                $("#Loading").removeClass("hide");
+            }
+            //data = (data == null || data == "" || typeof (data) == "undefined") ? { "date": new Date().getTime() } : data;
+            if (typeof (asyncC) != "undefined" && typeof (asyncC) != "boolean") {
+                $("#Loading").addClass("hide");
+                //GetAlert("同异步参数没传");
+            }
+            $.ajax({
+                type: "post",
+                data: data,
+                url: url + "?time=" + new Date().getTime(),
+                headers: this.GetCookie("admin") == "" ? null : eval('(' + this.GetCookie("admin") + ')'),
+                dataType: "json",
+                global: false,
+                async: typeof (asyncC) == "undefined" || null == asyncC ? true : false,
+                success: function (d) {
+                    self.GotoSpecialPage(d);
+                    successfn(d);
+                },
+                error: function (e) {
+                    //e={"readyState":0,"status":0,"statusText":"error"}
+                    //e={"readyState":4,"status":506,"statusText":"SessionTimeout"}
+                    if (e.status === 506) {
+                        //GetAlert("登录超时！");
+                        setTimeout("window.location ='" + window.appCtx['aist-sso-web'] + "/login?service=" + window.appCtx['ctm-web'] + "'", 2000);
+                    } else {
+                        errorfn("status:" + e.status + "-" + e.statusText);
+                    }
+                }
+            })
+        },
+       common.GetCookie = function (Name) {
+            var search = Name + "="//查询检索的值
+            var returnvalue = "";//返回值
+            if (document.cookie.length > 0) {
+                let sd = document.cookie.indexOf(search);
+                if (sd != -1) {
+                    sd += search.length;
+                    let end = document.cookie.indexOf(";", sd);
+                    if (end == -1)
+                        end = document.cookie.length;
+                    //unescape() 函数可对通过 escape() 编码的字符串进行解码。
+                    returnvalue = unescape(document.cookie.substring(sd, end))
                 }
             }
-        });
-    };
-
-    /**
-       * ajax封装
-       * url 发送请求的地址
-       * data 发送到服务器的数据，数组存储，如：{"date": new Date().getTime(), "state": 1}
-       * dataType 预期服务器返回的数据类型，常用的如：xml、html、json、text
-       * successfn 成功回调函数
-       * errorfn 失败回调函数
-       * asyncC:此为第五个参数传就是同步，不穿默认异步。
-    */
-    common.postWebJson = function (url, data, successfn, errorfn, asyncC, callLoading) {
-        if (!callLoading) {
-            $("#Loading").removeClass("hide");
+            return returnvalue;
         }
-        //data = (data == null || data == "" || typeof (data) == "undefined") ? { "date": new Date().getTime() } : data;
-        if (typeof (asyncC) != "undefined" && typeof (asyncC) != "boolean") {
-            $("#Loading").addClass("hide");
-            GetAlert("同异步参数没传");
-        }
-        $.ajax({
-            type: "post",
-            data: data,
-            url: url + "?time=" + new Date().getTime(),
-            dataType: "json",
-            global: false,
-            async: typeof (asyncC) == "undefined" || null == asyncC ? true : false,
-            success: function (d) {
-                successfn(d);
-            },
-            error: function (e) {
-                //e={"readyState":0,"status":0,"statusText":"error"}
-                //e={"readyState":4,"status":506,"statusText":"SessionTimeout"}
-                if (e.status === 506) {
-                    GetAlert("登录超时！");
-                    setTimeout("window.location ='" + window.appCtx['aist-sso-web'] + "/login?service=" + window.appCtx['ctm-web'] + "'", 2000);
-                } else {
-                    errorfn("status:" + e.status + "-" + e.statusText);
-                }
-            }
-        });
-    };
-
     //根据属性找对象
     common.FindObjByProp = function (arr, propName, value) {
         var newArr = [];
