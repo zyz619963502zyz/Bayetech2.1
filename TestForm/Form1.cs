dc40c5448c582318d6893a2b0e0f3b82b2c728c2
zhaoyz5 <619963502@qq.com>
2018-07-25 03:14:30 +0800

    tijiaoda

--- TestForm/Form1.cs
+++ TestForm/Form1.cs
@@ -16,19 +16,15 @@ namespace TestForm
 
         private void button1_Click(object sender, EventArgs e)
         {
-           
-
             int str = Convert.ToInt32(conditionText.Text);
-            WorkflowApplication app = new WorkflowApplication(new test(), new Dictionary<string, object>() {
-            {"TextInput",str }
-            });
+            WorkflowApplication app = 
+            new WorkflowApplication(new test(), new Dictionary<string, object>() {{"TextInput",str }});
             app.Run();
 
             //持久化操作的环节 
             string conStr = @"Server=.\MSSQLSERVER2012;database=WorkFlowDB;uid=sa;pwd=yujie1127";
             SqlWorkflowInstanceStore store = new SqlWorkflowInstanceStore(conStr);
             app.InstanceStore = store;
-
         }
 
         private void button2_Click(object sender, EventArgs e)
