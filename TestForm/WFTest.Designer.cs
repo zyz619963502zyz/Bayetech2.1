4882048675a242e8468c04da1b0939ae09e281a5
zhaoyz5 <619963502@qq.com>
2018-08-03 00:43:03 +0800

    提交代码

--- TestForm/WFTest.Designer.cs
+++ TestForm/WFTest.Designer.cs
@@ -32,6 +32,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.AppIdText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -51,6 +53,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "启动工作流";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
@@ -68,11 +71,29 @@
             this.button2.Text = "审批通过继续";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // AppIdText
+            // 
+            this.AppIdText.Location = new System.Drawing.Point(264, 194);
+            this.AppIdText.Name = "AppIdText";
+            this.AppIdText.Size = new System.Drawing.Size(145, 25);
+            this.AppIdText.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(125, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "操作值：";
+            // 
             // WFTest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(659, 495);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.AppIdText);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -90,5 +111,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox AppIdText;
+        private System.Windows.Forms.Label label2;
     }
 }
