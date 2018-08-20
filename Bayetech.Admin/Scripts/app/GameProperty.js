ebf3c9dec1847a9ece92183b836654675164e9e1
Bayetech_zyz <619963502@qq.com>
2018-08-20 09:06:40 +0800

    提交代码

--- Bayetech.Admin/Scripts/app/GameProperty.js
+++ Bayetech.Admin/Scripts/app/GameProperty.js
@@ -2,44 +2,37 @@
 import comCompnent from '../common.js'
 import componentTable from '../components/table-Process.vue'
 
-let pagetype = comCompnent.GetUrlParam($(".NFine_iframe").context.URL,"type");
-
 let vmData = {
-    //BaseUrl: GetBaseUrl()+"Good/GoodInfo.html?GoodNo=",
-    PageType:pagetype,//待处理，已处理，24小时未处理等等单据类型。
-    ItemType:"good",//单据类型
+    GameListUrl: "/api/Game/",
+    GetByLetterUrl:"/api/Game/GetGameListByLetter",
     tools:{
         _comCompnent:comCompnent,
         _componentTable:componentTable
     },
-    GoodListUrl:comCompnent.MenuUrl[pagetype],
-    CheckGoodUrl:"/api/CheckGood/CheckGoodInfo",
-    CheckGoodNo:"",//模态框打开的GoodNo
-    keyword: "",
-    GoodInfoArray:[],
+    Letters: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],//首字母集合
+    GameArray:[],
     ListObj: [
         {
-            GoodNo: "",
-            GoodFirstPicture: "",
-            aurl: "",
-            GoodTitle: "",
-            GroupName: "",
-            ServerName: "",
-            GoodPrice: ""
+            Name: "",
+            Alias: "",
+            Letter: "",
+            IsHot: "",
+            Parentid: "",
+            Order: "",//排序
+            Platform: "",
+            Img:"",
+            IsDelete:""//是否被删除
         }
     ],
     SearchParam: {
         Param: {//查询条件的参数
-            GoodNo:"",
-            OrderNo:"",
-            Status:pagetype,
-            SelectType:"good",//form里选择的商品类型
-            SelectNo:""//form里面选择的编号
+            SelectLetter:"",//选择的游戏首字母
+            SelectGame:""//选择的游戏
         },
         Pagination: {//分页对象
             rows: 10,//每页行数，
             page: 1,//当前页码
-            order: "GoodNo",//排序字段
+            order: "IsHot",//排序字段
             sord: "asc",//排序类型
             records: 10,//总记录数
             total: 10//总页数。
@@ -48,45 +41,39 @@ let vmData = {
 };
 
 new Vue({
-    el: '#CommForm',
+    el: '#app',
     data: vmData,
     created(){
-        this.findList();
+        //this.findList();
     },
     methods: {
         findList() {//获取商品的简要列表
-            $("#QueryList").Btns("loading");
-            var self=this;
-            self.SearchParam.Param.SelectType =="good"? (self.SearchParam.Param.GoodNo = self.SearchParam.Param.SelectNo):
-            (self.SearchParam.Param.OrderNo = self.SearchParam.Param.SelectNo,self.SearchParam.Param.GoodNo ="");//如果是订单把商品编号置空。
-            //后台传值：
-            self.tools._comCompnent.postWebJson(self.GoodListUrl, self.SearchParam, function (data) {
+            self.tools._comCompnent.postWebJson(self.GameListUrl, self.SearchParam, function (data) {
                 $("#QueryList").Btns("reset");
                 if (data.result) {
-                    self.GoodInfoArray=data.content.datas;
-                    self.ItemType = self.SearchParam.Param.SelectType;//根据单据类型选择加载的标题等等内容
-                    self.SearchParam.Pagination=data.content.pagination;
-                    self.tools._comCompnent.SetPagination($('#paginator-test'), self.SearchParam, self.findList);
+                    self.SearchParam.Pagination = data.content.pagination;
+                    self.tools._comCompnent.SetPagination($('#paginator-test'),
+                        self.SearchParam, self.findList);
                 }
             },function(){
                 $("#QueryList").Btns("reset");
             });
         },
-        StartCheck(GoodNo) {//开始检查
+        GetByLetter(letter){//根据首字母去查询下拉游戏
             var self = this;
-            self.CheckGoodNo = GoodNo;
-            $("#checkModal").modal("show");
+            self.tools._comCompnent.getWebJson(self.GetByLetterUrl, {"type":0,"letter":letter}, function (data) {
+                self.GameArray = data.content;
+            });
         },
         TurnToPage(page){
             var self = this;
             self.SearchParam.Pagination.rows = page;
             self.findList();
-        }
+        },
     },
     components:{
         comtable:componentTable
     }
 });
 
-//剩余功能缺乏鉴定前后端加载的是订单还是商品的逻辑。后台。
 
