7e4f4319f6521155bf782e5f532ced4dc7286843
wuwb07 <wuwb07@centaline.com>
2018-09-14 19:34:53 +0800

    修改数据库链接

--- TestForm/Form1.cs
+++ TestForm/Form1.cs
@@ -21,7 +21,7 @@ namespace TestForm
             app.Run();
 
             //持久化操作的环节 
-            string conStr = @"Server=115.159.211.34;database=WF;uid=sa;pwd=sh.1234";
+            string conStr = @"Server=47.98.176.184;database=WF;uid=sa;pwd=sh.1234";
             SqlWorkflowInstanceStore store = new SqlWorkflowInstanceStore(conStr);
             app.InstanceStore = store;
         }
