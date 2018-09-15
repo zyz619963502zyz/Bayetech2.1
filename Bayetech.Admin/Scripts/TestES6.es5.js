87a4bcff58924fa755487882b581049126949123
zhaoyz5 <619963502@qq.com>
2018-09-15 09:37:00 +0800

    提交所有diamante

---
+++ Bayetech.Admin/Scripts/TestES6.es5.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+    value: true
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _commonJs = require('./common.js');
+
+var _commonJs2 = _interopRequireDefault(_commonJs);
+
+var a = 1;
+var c = 2;
+function jisuan() {
+    return a * c;
+}
+
+exports.jisuan = jisuan;
+
