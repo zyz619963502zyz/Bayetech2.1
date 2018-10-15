2d26dc5da45af8ac303f3ec9461c17c790615d42
Bayetech_zyz <619963502@qq.com>
2018-10-15 20:16:04 +0800

    提交新增加的代码

--- TestForm/Program.cs
+++ TestForm/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TestForm
@@ -17,6 +14,10 @@ namespace TestForm
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new TestForm());
+
+
+            //XAML读取
+            
         }
     }
 }
