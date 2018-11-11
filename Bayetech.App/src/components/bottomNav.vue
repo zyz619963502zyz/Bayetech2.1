5ae630a7f6d649f2b5b2dd6ca76c7a6deff79127
liuzy001 <kakaliu_001@163.com>
2018-11-11 17:32:34 +0800

    提交

--- Bayetech.App/src/components/bottomNav.vue
+++ Bayetech.App/src/components/bottomNav.vue
@@ -3,32 +3,31 @@
   
   <div data-style="fixed-bottom" class="newfooter fixed-bottom">
     <ul>
-      <a href="/" class="foot-home">
-        <li class="">
+      <li class="">
+        <a href="#" class="foot-home">
           <span class="i_home">主页</span>
-        </li>
-      </a>
-      <a href="/vue/gameList/2" class="foot-home">
-        <li class="">
+        </a>
+      </li>
+      <li class="">
+        <a href="#" class="foot-home">
           <span class="i_mybuy">我要买</span>
-        </li>
-      </a>
-      <a href="/vue/sell-home/1" class="foot-home">
-        <li class="">
+        </a>
+      </li>
+      <li class="">
+        <a href="#" class="foot-home">
           <span class="i_mysell">我要卖</span>
-        </li>
-      </a>
-      <a href="/vue/message" class="foot-message">
-        <li class="">
+        </a>
+      </li>
+      <li class="">
+        <a href="#" class="foot-message">
           <span class="i_message">消息</span>
-        </li>
-      </a>
-      <a href="/vue/center" class="foot-home">
-        <li class="">
+        </a>
+      </li>
+      <li class="">
+        <router-link :to="{path:'/center'}" class="foot-home">
           <span class="i_tx">我的</span>
-          <!---->
-        </li>
-      </a>
+        </router-link>
+      </li>
     </ul>
   </div>
 
