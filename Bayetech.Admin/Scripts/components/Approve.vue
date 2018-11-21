01e4d0a0752185fe2d967a4863e0b6e1bc73e2d1
zhaoyz <619963502@qq.com>
2018-11-22 00:06:38 +0800

    提交审批对象

--- Bayetech.Admin/Scripts/components/Approve.vue
+++ Bayetech.Admin/Scripts/components/Approve.vue
@@ -41,31 +41,30 @@
                     DispUserInfo: comCompnent.default.EngineUrl + "Get_DispUserInfo",
                 },
                 Param: {//参数
-                    NewFlowExample: {
-                        Flow_Id: "",
-                        Cur_Status_Id: "",
-                        New_Status_Id: "",
-                        Reciever_Id: "",
-                        Sender_Id: "",
-                        Send_Time:""
+                    NewFlowExample: {//新建流程实例
+                        EngineInfo: JSON.parse(JSON.stringify(comCompnent.default.EngineInfo)),//对象深拷贝
                     },
-                    FlowBeginStatusInfo: {
-
+                    OnNextStep: {//提交送下一步
+                        EngineInfo: JSON.parse(JSON.stringify(comCompnent.default.EngineInfo)),//对象深拷贝
+                        PageInfo: JSON.parse(JSON.stringify(comCompnent.default.PageInfo))
                     },
-                    OnNextStep: {
-
+                    FlowBeginStatusInfo: {//获取当前流程的第一个环节
+                        flowId:"",
                     },
-                    FlowStatusInfo: {
-
+                    FlowStatusInfo: {//获取流程当前环节
+                        wfmId:""
                     },
-                    PermList: {
-
+                    PermList: {//获取流程权限
+                        lngPerm:""//权限累加算出来的值
                     },
-                    StatusAllDisposal: {
-
+                    StatusAllDisposal: {//获取当前环节流程线
+                        p_lFlow_ID: "",//流程ID
+                        p_lStatus_ID: "",//环节ID
+                        PageConditionRule:""//页面规则
                     },
-                    DispUserInfo: {
-
+                    DispUserInfo: {//获取流程线 所有人员(无数据权限)
+                        p_lFlow_ID: "",
+                        p_lDisosal_ID:""
                     }
                 },
                 ResultList: {
@@ -88,12 +87,12 @@
         },
         mounted() {
             var self = this;
-            self.Get_PermList();
+            //self.Get_PermList();
         },
         methods: {
             Create_NewFlowExample() {//创建流程实例
                 var self = this;
-                comCompnent.default.getWebJson(self.NewFlowExampleUrl, param, function (data) {
+                comCompnent.default.getWebJson(self.NewFlowExampleUrl, self.Param.NewFlowExample, function (data) {
                     if (data) {
                         self.NewFlowExampleList = data;
                         alert("实例创建成功!");
@@ -102,7 +101,7 @@
             },
             Get_FlowBeginStatusInfo() {//获取流程开始环节信息
                 var self = this;
-                comCompnent.default.getWebJson(self.FlowBeginStatusInfoUrl, param, function (data) {
+                comCompnent.default.getWebJson(self.FlowBeginStatusInfoUrl, self.Param.FlowBeginStatusInfo, function (data) {
                     if (data) {
                         self.FlowBeginStatusInfoList = data;
                         alert("返回成功!");
@@ -111,7 +110,7 @@
             },
             Execute_OnNextStep() {//提交送下一步
                 var self = this;
-                comCompnent.default.getWebJson(self.OnNextStepUrl, param, function (data) {
+                comCompnent.default.getWebJson(self.OnNextStepUrl, self.Param.OnNextStep, function (data) {
                     if (data) {
                         self.FlowBeginStatusInfoList = data;
                         alert("返回成功!");
@@ -120,7 +119,7 @@
             },
             Get_FlowStatusInfo() {//获取当前流程及环节信息
                 var self = this;
-                comCompnent.default.getWebJson(self.FlowStatusInfoUrl, param, function (data) {
+                comCompnent.default.getWebJson(self.FlowStatusInfoUrl, self.Param.FlowStatusInfo, function (data) {
                     if (data) {
                         self.FlowStatusInfoList = data;
                         alert("返回成功!");
@@ -129,7 +128,7 @@
             },
             Get_PermList() {//获取权限
                 var self = this;
-                comCompnent.default.getWebJson(self.PermListUrl, null, function (data) {
+                comCompnent.default.getWebJson(self.PermListUrl, self.Param.PermList, function (data) {
                     if (data) {
                         self.PermList = data;
                         alert("返回成功!");
@@ -143,7 +142,7 @@
                     p_lFlow_ID: "1",
                     PageConditionRule: ""
                 }
-                comCompnent.default.getWebJson(self.StatusAllDisposalUrl, param, function (data) {
+                comCompnent.default.getWebJson(self.StatusAllDisposalUrl, self.Param.StatusAllDisposal, function (data) {
                     if (data) {
                         self.StatusAllDisposalList = data;
                         alert("返回成功!");
@@ -152,7 +151,7 @@
             },
             Get_DispUserInfo() {//获取下一处理人
                 var self = this;
-                comCompnent.default.getWebJson(self.DispUserInfoUrl, data, function (data) {
+                comCompnent.default.getWebJson(self.DispUserInfoUrl, self.Param.DispUserInfo, function (data) {
                     if (data) {
                         self.DispUserInfoList = data;
                         alert("返回成功!");
