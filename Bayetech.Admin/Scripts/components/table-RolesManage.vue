ecb20be32ef5d053013c3fbd7b8ef2c0d6a421ec
桂源 张 <786953742@qq.com>
2018-12-10 23:15:53 +0800

    1

--- Bayetech.Admin/Scripts/components/table-RolesManage.vue
+++ Bayetech.Admin/Scripts/components/table-RolesManage.vue
@@ -8,18 +8,20 @@
             <tr class="success">
                 <th class="text-center col-md-1">编号</th>
                 <th class="text-center col-md-2">角色名称</th>
-                <th class="text-center col-md-2">排序</th>
+                <th class="text-center col-md-2">模块</th>
+                <th class="text-center col-md-2">简写</th>
                 <th class="text-center col-md-4">描述</th>
-				<th class="text-center col-md-2">操作</th>
+                <th class="text-center col-md-2">操作</th>
             </tr>
         </thead>
         <tbody v-for="item in rolesarray">
             <tr class="text-center">
-                <td class="text-center"><label class="table-firsttitle">{{item.KeyId}}</label></td>
-                <td>{{item.RoleName}}</td>
-                <td>{{item.Sortnum}}</td>
-                <td>{{item.Remark}}</td>
-				<td><input id="test" type="radio" name="Operates" value=""  @click="startcheck(item)"/></td>
+                <td class="text-center"><label class="table-firsttitle">{{item.Role_id}}</label></td>
+                <td>{{item.Role_Name}}</td>
+                <td>{{item.Module_ID}}</td>
+                <td>{{item.Role_Remark}}</td>
+                <td>{{item.RoleSerial}}</td>
+                <td><input id="test" type="radio" name="Operates" value="" @click="startcheck(item)" /></td>
             </tr>
         </tbody>
     </table>
