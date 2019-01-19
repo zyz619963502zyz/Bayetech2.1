44c8b7c784dc20df5c3ab7731b1e77ece41afaa7
liuzy001 <kakaliu_001@163.com>
2019-01-19 16:55:14 +0800

    test

--- Bayetech.App/src/App.vue
+++ Bayetech.App/src/App.vue
@@ -5,19 +5,17 @@
 </template>
 
 <script>
- 
+
 export default {
-    name: 'App',
-    metaInfo:{
-        meta:[
-            {'http-equiv':'Content-Type',content:'text/html;charset=UTF-8'},
-            {'http-equiv':'X-UA-Compatible',content:'IE=edge'},
-            {name:'apple-mobile-web-app-capable',content:'yes'},
-            {name:'apple-mobile-web-app-status-bar-style',content:'black'},
-            {name:'format-detection',content:'telephone=no, email=no'}
-        ]
-    } 
+  name: 'App',
+  metaInfo: {
+    meta: [
+      {'http-equiv': 'Content-Type', content: 'text/html;charset=UTF-8'},
+      {'http-equiv': 'X-UA-Compatible', content: 'IE=edge'},
+      {name: 'apple-mobile-web-app-capable', content: 'yes'},
+      {name: 'apple-mobile-web-app-status-bar-style', content: 'black'},
+      {name: 'format-detection', content: 'telephone=no, email=no'}
+    ]
+  }
 }
 </script>
-
-
