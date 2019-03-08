96557f3bb8a83498806b6ab4b8fe8b0ef72f2c80
Liuzy34 <kakaliu_001@163.com>
2019-03-08 20:51:06 +0800

    按需修改

--- Bayetech.Web/Scripts/app/Shared/nav.js
+++ Bayetech.Web/Scripts/app/Shared/nav.js
@@ -3,15 +3,17 @@ define([], function () {
     var html = `<div class="nav-box"><ul class="nav nav-pills nav-justified" role="tablist">
                 <li class="active"><a href="#home0">首页</a></li>
                 <li><a href="../Game/List.html" target="_blank">金币交易</a></li>
-                <li><a href="../Game/List.html" target="_blank">账号交易</a></li>
                 <li><a href="../Game/List.html" target="_blank">装备交易</a></li>
-                <li><a href="#">游戏代练</a></li>
+                <li><a href="#">游戏攻略</a></li>
+                <li><a href="#">关于我们</a></li>
+                <li><a href="#">充值</a></li>
             </ul></div>`;
-
+    //  <li><a href="../Game/List.html" target="_blank">账号交易</a></li>
+    //  <li><a href="#">游戏代练</a></li>
     var data = {
         object: [{
             title: "账号交易",
-            url: "http://www.7881.com/gamelistshow.html",
+            url: "",
         }, ],
     }
 
