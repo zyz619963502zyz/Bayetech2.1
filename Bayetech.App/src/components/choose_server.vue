9c807b645bb9fd8f16da1f1613c038da93fa3959
武文彬 <wuwenbin@huazhu.com>
2019-03-20 00:32:26 +0800

    选择区服组件

--- Bayetech.App/src/components/choose_server.vue
+++ Bayetech.App/src/components/choose_server.vue
@@ -1,84 +1,218 @@
 <!--底部导航-->
 <template>
-  
-  <div data-v-7b64404c="" data-v-6b09e788="" v-show="conditionTab.server">
-    <div data-v-7b64404c="" class="seach-list">
-      <div data-v-7b64404c="" class="gmvst-profile ">
-        <div data-v-7b64404c="" class="gmvst-server fl">
-          <ul data-v-7b64404c="">
-            <!---->
-            <li data-v-7b64404c="" class="border-bottom curren">
-              <a data-v-7b64404c="" @click='setValue({GameGroupName:""})'>
-                <span data-v-7b64404c="" class="f30 color-000" v-show="!searchModel.GameGroupName">
-                  选择区
-                  <i data-v-7b64404c="" class="close"></i>
-                </span>
-                <span data-v-7b64404c="" class="f30 color-000" v-show="!!searchModel.GameGroupName">
-                  {{searchModel.GameGroupName}}
-                  <i data-v-7b64404c="" class="close"></i>
-                </span>
-              </a>
+
+  <div data-v-4d8e3562="" data-v-484bcaae="">
+    <div data-v-4d8e3562="" class="one-screen">
+      <div data-v-4d8e3562="" style="z-index: 100;">
+        <div data-v-4d8e3562="" class="top-header border-bottom">
+          <div data-v-4d8e3562="" class="top-back">
+            <a data-v-4d8e3562="" @click="CloseDialog"></a>
+          </div>
+          <h2 data-v-4d8e3562="" class="f36">选择服务器</h2>
+          <div data-v-4d8e3562="" class="top-right"></div>
+        </div>
+      </div>
+      <div data-v-4d8e3562="" class="search-container">
+        <div data-v-4d8e3562="" class="search-box bg-fff">
+          <input data-v-4d8e3562="" type="text" placeholder="请输入服务器名称" maxlength="20" class="f30">
+          <span data-v-4d8e3562="" class="clear1" style="display: none;"></span>
+        </div>
+      </div>
+      <div data-v-4d8e3562="" class="qufu-container f30 clearfix border-top">
+        <div data-v-4d8e3562="" class="qu fl" style="height: 100%;">
+          <ul data-v-4d8e3562="">
+            <li data-v-4d8e3562="" class="selected">
+              <span data-v-4d8e3562="">广东区</span>
             </li>
-            <li data-v-7b64404c="" class="border-bottom">
-              <a data-v-7b64404c="">
-                <span data-v-7b64404c="" class="f30 color-000">
-                  选择服
-                  <i data-v-7b64404c="" class="close"></i>
-                </span>
-              </a>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">北京区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">四川区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">江苏区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">湖北区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">广西区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">东北区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">浙江区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">山东区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">上海区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">西南区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">安徽区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">河北区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">辽宁区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">福建区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">华北区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">重庆区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">黑龙江区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">江西区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">西北区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">陕西区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">吉林区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">山西区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">天津区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">河南区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">湖南区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">新疆区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">内蒙古区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">云贵区</span>
+            </li>
+            <li data-v-4d8e3562="" class="">
+              <span data-v-4d8e3562="">体验区</span>
             </li>
           </ul>
         </div>
-        <div data-v-7b64404c="" class="gmvst-area bg-fff">
-          <div data-v-7b64404c="" class="gmvst-server">
-            <div data-v-7b64404c="" class="gvst-seach mx-30 my-20 border py-15 px-20">
-              <input data-v-7b64404c="" name="" type="text" placeholder="请输入服务器名称" class="server-input" v-model="serverKeyWords">
-            </div>
-          </div>
-          <ul data-v-7b64404c="" class="pl-30" v-show="groupId">
-            <a @click='setValue({GameGroupName:group.Name,GameGroupId:group.Id})' v-for='group in filterGroupList' v-bind:key="group.Id">
-              <li data-v-7b64404c="" class="border-bottom">
-                <span data-v-7b64404c="" class="f30 color-000">{{group.Name}}</span>
-              </li>
-            </a>
-
-            <li data-v-7b64404c="" class="border-bottom">
-              <span data-v-7b64404c="" class="f30 color-000">体验区</span>
+        <div data-v-4d8e3562="" class="fu bg-fff fl" style="height: 100%;">
+          <ul data-v-4d8e3562="">
+            <li data-v-4d8e3562="" style="text-align: left; padding-left: 0.3rem;">
+              <span data-v-4d8e3562="" class="border-bottom">广东1区</span>
             </li>
-          </ul>
-          <ul data-v-7b64404d="" class="pl-30" v-show="!!searchModel.GameGroupName">
-            <a @click="setValue({GameServerName:server.Name,GameServerId:server.Id});ToggleConditionTab('server');SearchList();" v-for='server in filterServerList' v-bind:key="server.Id">
-              <li data-v-7b64404c="" class="border-bottom">
-                <span data-v-7b64404c="" class="f30 color-000">{{server.Name}}</span>
-              </li>
-            </a>
-
-            <li data-v-7b64404c="" class="border-bottom">
-              <span data-v-7b64404c="" class="f30 color-000">体验区</span>
+            <li data-v-4d8e3562="" style="text-align: left; padding-left: 0.3rem;">
+              <span data-v-4d8e3562="" class="border-bottom">广东2区</span>
+            </li>
+            <li data-v-4d8e3562="" style="text-align: left; padding-left: 0.3rem;">
+              <span data-v-4d8e3562="" class="border-bottom">广东3区</span>
+            </li>
+            <li data-v-4d8e3562="" style="text-align: left; padding-left: 0.3rem;">
+              <span data-v-4d8e3562="" class="border-bottom">广东4区</span>
+            </li>
+            <li data-v-4d8e3562="" style="text-align: left; padding-left: 0.3rem;">
+              <span data-v-4d8e3562="" class="border-bottom">广东5区</span>
+            </li>
+            <li data-v-4d8e3562="" style="text-align: left; padding-left: 0.3rem;">
+              <span data-v-4d8e3562="" class="border-bottom">广东6区</span>
+            </li>
+            <li data-v-4d8e3562="" style="text-align: left; padding-left: 0.3rem;">
+              <span data-v-4d8e3562="" class="border-bottom">广东7区</span>
+            </li>
+            <li data-v-4d8e3562="" style="text-align: left; padding-left: 0.3rem;">
+              <span data-v-4d8e3562="" class="border-bottom">广东8区</span>
+            </li>
+            <li data-v-4d8e3562="" style="text-align: left; padding-left: 0.3rem;">
+              <span data-v-4d8e3562="" class="border-bottom">广东9区</span>
+            </li>
+            <li data-v-4d8e3562="" style="text-align: left; padding-left: 0.3rem;">
+              <span data-v-4d8e3562="" class="border-bottom">广东10区</span>
+            </li>
+            <li data-v-4d8e3562="" style="text-align: left; padding-left: 0.3rem;">
+              <span data-v-4d8e3562="" class="border-bottom">广东11区</span>
+            </li>
+            <li data-v-4d8e3562="" style="text-align: left; padding-left: 0.3rem;">
+              <span data-v-4d8e3562="" class="border-bottom">广东12区</span>
+            </li>
+            <li data-v-4d8e3562="" style="text-align: left; padding-left: 0.3rem;">
+              <span data-v-4d8e3562="" class="border-bottom">广东13区</span>
+            </li>
+            <li data-v-4d8e3562="" style="text-align: left; padding-left: 0.3rem;">
+              <span data-v-4d8e3562="" class="border-bottom">广州1/2区</span>
             </li>
           </ul>
         </div>
       </div>
+      <div data-v-4d8e3562="" class="search-result f28" style="display: none;">
+        <ul data-v-4d8e3562=""></ul>
+      </div>
+      <div data-v-4d8e3562="" class="search-empty" style="display: none;">
+        <img data-v-4d8e3562="" src="" alt="">
+        <p data-v-4d8e3562="">
+          抱歉，没有找到“
+          <span data-v-4d8e3562="" class="search-content"></span>”的相关结果
+        </p>
+      </div>
+      <div data-v-4d8e3562="" class="mask" style="display: none;"></div>
+      <div data-v-4d8e3562="" class="search-history" style="display: none;">
+        <div data-v-4d8e3562="" class="container-ms bg-fff">
+          <div data-v-4d8e3562="" class="game-list ">
+            <ul data-v-4d8e3562="" class="clearfix ">
+              <li data-v-4d8e3562="">
+                <a data-v-4d8e3562="">暂无搜索历史</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-    <!---->
-    <div data-v-7b64404c="" class="shade" style="display:none"></div>
   </div>
 
 </template>
 
 <script>
-
+  import "@/assets/css/goods-publish-new.css";
   export default{
     data() {
       return {
-        groupId:'',
-        serverId: '',
+        chooseServer: true,
+        serverKeyWords:'',
+        groupName: '',
+        groupId: '',
+        serverName: '',
+        serverId:'',
+        cs_server: {
+          filterGroupList: [''],
+          filterServerList: [''],
+          groupNameList:[''],
+          groupIdList: [''],
+          serverNameList: [''],
+          serverIdList: ['']
+        },
+
         serverList: [{ Id: 0, Name: "上海1区" }, { Id: 1, Name: "上海2区" }],
-        groupList: [{ Id: 0, Name: "上海区" }, { Id: 1, Name: "广东区" }],
+        groupList: [{ Id: 0, Name: "上海区" }, { Id: 1, Name: "广东区" }]
       }
     },
     watch: {},
-    method: {
+    methods: {
       Init: function (groupId, serverId) {
         let self = this;
         this.serverId = serverId;
@@ -89,9 +223,12 @@
           GetServer(self.groupId);
         }
       },
+      CloseDialog: function () {
+        this.$emit('cancel');
+      },
       Choose_server: function () {
 
-        $.emit('update_server', data)
+        this.$emit('submit', data)
       },
       GetGroup: function () {
         let self = this;
@@ -125,7 +262,7 @@
     props:['pagetype','goodinfoarray','startcheck','itemtype']
 }
 </script>
-
 <style>
 
 </style>
+
