b125a08f8a08dad2670d2109a74f98c59d5f08c5
张桂源 <zhangguiyuan@fineex.com>
2019-05-13 17:48:09 +0800

    增加商品发布页面

---
+++ Bayetech.Web/Controllers/NoFollowController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bayetech.Web.Controllers
+{
+    public class NoFollowController : Controller
+    {
+        // GET: NoFollow
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
